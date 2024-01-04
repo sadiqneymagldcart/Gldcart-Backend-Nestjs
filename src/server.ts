00229bb1cbc0b90435edbf26df1c5c01ac859aeb
sappChak <konotop401@gmail.com>
2024-01-04 21:21:00 +0100

    Remove selfsigned package and return to HTTP server
    The code is altered to remove the 'selfsigned' package which was initially used to generate self-signed certificates for a HTTPS server. The functionality to run a secure HTTPS server is reverted and the app now listens on HTTP again.

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,6 @@
 import {app} from "./app";
-import https from 'https'
-import selfsigned from 'selfsigned'
-
-const pems = selfsigned.generate([{ name: 'commonName', value: 'localhost' }]);
-const credentials = {key: pems.private, cert: pems.cert};
-
 const port: number | string = process.env.PORT || 5000;
 
-const httpsServer = https.createServer(credentials, app);
-
-httpsServer.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
