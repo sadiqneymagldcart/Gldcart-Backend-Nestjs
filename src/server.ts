b145cd2ea9aac193d598a9e7838d3e033b435e52
sappChak <konotop401@gmail.com>
2024-01-04 21:05:46 +0100

    Implement HTTPS server for secure cookies
    Added an HTTPS server with self-signed certificates to improve the security of cookies transferred between the server and the client. This change makes communication secure and helps to prevent any potential cookie theft or hijacking. The server now listens to https, providing secure handling of data transmission.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,14 @@
 import {app} from "./app";
+import https from 'https'
+import selfsigned from 'selfsigned'
+
+const pems = selfsigned.generate([{ name: 'commonName', value: 'localhost' }]);
+const credentials = {key: pems.private, cert: pems.cert};
+
 const port: number | string = process.env.PORT || 5000;
 
-app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
 });
