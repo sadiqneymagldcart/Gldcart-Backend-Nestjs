28011e683a9ca82e15cb9254928a776c0b16a749
sappChak <konotop401@gmail.com>
2024-01-08 18:45:33 +0100

    Refactor configuration and routes setup in application
    The configuration, routes setup, and MongoDB startup logic were refactored out of the main entry point to improve code organization and readability. This implies that their respective logic has been moved to separate 'configureRoutes.ts', 'middlewares.config.ts', and 'mongo.config.ts' files. In turn, in 'app.ts', the functions 'setupMiddlewares' and 'startMongoDb' were imported and called to perform these actions

---
+++ src/config/middlewares.config.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import {appConfig} from "./appConfig";
+import {configureRoutes} from "./configureRoutes";
+import errorMiddleware from "../middlewares/errorMiddleware";
+
+export function setupMiddlewares(app: express.Express) {
+    app.set("trust proxy", 1);
+    app.use(helmet());
+    app.use(
+        rateLimit({
+            windowMs: 15 * 60 * 1000,
+            limit: 200,
+        })
+    );
+    app.use('/webhook', bodyParser.raw({type: "*/*"}));
+    app.use(express.json());
+    app.use(
+        express.urlencoded({
+            extended: true,
+        })
+    );
+    app.use(cookieParser());
+    app.use(
+        cors({
+            origin: appConfig.CLIENT_URL,
+            credentials: true,
+        })
+    );
+    configureRoutes(app);
+
+    app.use(errorMiddleware);
+}
