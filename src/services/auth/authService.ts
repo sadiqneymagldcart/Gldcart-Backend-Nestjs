97cb45dab743677882abc59bf9c203348e14cfc2
sappChak <konotop401@gmail.com>
2024-01-21 19:19:45 +0100

    Remove multiple obsolete files and features
    Deleted several files and associated references across the codebase including Address, Cart, and Google OAuth related files. The removal of these files reflects a structural change in the application targeting cleaner and more efficient code maintenance. Some files like 'app.ts', 'authRoutes.ts', product related files and others, have been removed painting towards a significant architectural change in the project.

---
+++ src/services/auth/authService.ts
@@ -0,0 +1,108 @@
+import { TokenService } from "../token/tokenService";
+import { Logger } from "../../utils/logger";
+import { BaseService } from "../baseService";
+import { ApiError } from "../../exceptions/api-error";
+import { UserDto } from "../../dtos/user-dto";
+import { IToken } from "../../models/Token";
+import User, { IUser } from "../../models/User";
+import bcrypt from "bcrypt";
+import { inject, injectable } from "inversify";
+
+@injectable()
+export class AuthService extends BaseService {
+    private tokenService: TokenService;
+    constructor(
+        @inject(TokenService) tokenService: TokenService,
+        @inject(Logger) logger: Logger,
+    ) {
+        super(logger);
+        this.tokenService = tokenService;
+    }
+
+    public async register(
+        type: string,
+        name: string,
+        surname: string,
+        email: string,
+        password: string,
+    ) {
+        if (await this.doesUserExist(email)) {
+            throw ApiError.BadRequest("That mail is already registered");
+        }
+        const hashedPassword: string = await this.hashPassword(password);
+        const user: IUser = <IUser>await User.create({
+            type,
+            name,
+            surname,
+            email,
+            password: hashedPassword,
+        });
+        return this.formUserLoginResponse(user, `User registered: ${email}`);
+    }
+
+    public async login(email: string, password: string) {
+        const user: IUser = <IUser>await User.findOne({ email });
+        if (user) {
+            const auth: boolean = await bcrypt.compare(password, user.password);
+            if (auth) {
+                return this.formUserLoginResponse(user, `User logged in: ${email}`);
+            }
+            await this.logger.logError(`Incorrect password for ${email}`);
+            throw ApiError.BadRequest("Incorrect password");
+        }
+        await this.logger.logError(`User not found with email: ${email}`);
+        throw ApiError.BadRequest("Incorrect mail");
+    }
+
+    public async logout(refreshToken: string) {
+        await this.logger.logInfo(`User logged out`);
+        return await this.tokenService.removeToken(refreshToken);
+    }
+
+    public async refresh(refreshToken: string) {
+        if (!refreshToken) {
+            await this.logger.logError("There is no refresh token");
+            throw ApiError.UnauthorizedError();
+        }
+        const userData = this.tokenService.validateRefreshToken(refreshToken);
+
+        const tokenFromDb: IToken | null =
+            await this.tokenService.findToken(refreshToken);
+        if (!userData || !tokenFromDb) {
+            await this.logger.logError("Refresh token is invalid");
+            throw ApiError.UnauthorizedError();
+        }
+        const user = <IUser>await User.findById(userData.id);
+        return this.formUserLoginResponse(user);
+    }
+
+    private async doesUserExist(email: string): Promise<boolean> {
+        const existingUser = await User.findOne({ email });
+        return Boolean(existingUser);
+    }
+
+    private async hashPassword(password: string): Promise<string> {
+        try {
+            const salt: string = await bcrypt.genSalt();
+            return await bcrypt.hash(password, salt);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    private async formUserLoginResponse(user: IUser, logMessage?: string) {
+        try {
+            const userDto: UserDto = new UserDto(user);
+            const tokens: { accessToken: string; refreshToken: string } =
+                this.tokenService.createTokens({ ...userDto });
+            await this.tokenService.saveToken(user.id, tokens.refreshToken);
+            if (logMessage) {
+                await this.logger.logInfo(logMessage);
+            }
+            return { ...tokens, user };
+        } catch (error: any) {
+            await this.logger.logError("kokos", error);
+            throw error;
+        }
+    }
+}
---
+++ src/services/user_details/userDetailsService.ts
@@ -0,0 +1,94 @@
+import {BaseService} from "../baseService";
+import {ApiError} from "../../exceptions/api-error";
+import {Logger} from "../../utils/logger";
+import {IAddress} from "../../models/Address";
+import User, {IUser} from "../../models/User";
+import {Types} from "mongoose";
+
+export class UserDetailsService extends BaseService{
+
+    constructor(logger:Logger) {
+        super(logger);
+    }
+    public async addAddress(userId: string, addressData: IAddress) {
+        const user = await User.findById(userId);
+
+        if (!user) {
+            await this.logger.logError(`User ${userId} not found while adding address`);
+            throw ApiError.BadRequest("User not found");
+        }
+
+        const newAddressId = new Types.ObjectId();
+        const addressWithId = {...addressData, id: newAddressId};
+
+        user.addresses.push(addressWithId);
+        await user.save();
+        await this.logger.logInfo(`Address added for user ${userId}`, user.addresses);
+        return user;
+    }
+
+     public async updateAddress(userId: string, addressId: Types.ObjectId, addressData: IAddress) {
+        const user: IUser | null = await User.findById(userId);
+
+        if (!user) {
+            await this.logger.logError(`User ${userId} was not found while updating address for email`);
+            throw ApiError.BadRequest('User was not found');
+        }
+        const addressIndex = user.addresses.findIndex(address => String(address.id) === String(addressId));
+        if (addressIndex === -1) {
+            await this.logger.logError(`Address was not found for user ${userId}`);
+            throw ApiError.BadRequest('Address was not found');
+        }
+        Object.assign(user.addresses[addressIndex], addressData);
+        await user.save();
+        await this.logger.logInfo(`Address was updated for user ${userId}`);
+    }
+
+    async getAddresses(id: string) {
+        const user: IUser | null = await User.findById(id);
+        if (!user) {
+            await this.logger.logError(`User not found while fetching addresses for ID: ${id}`);
+            throw ApiError.BadRequest('User not found');
+        }
+        return user.addresses;
+    }
+
+    async deleteAddress(userId: string, addressId: Types.ObjectId) {
+        const user = await User.findById(userId);
+        if (!user) {
+            await this.logger.logError(`User ${userId} not found while deleting address for email`);
+            throw ApiError.BadRequest("User not found");
+        }
+
+        const addressIndex = user.addresses.findIndex(address => String(address.id) === String(addressId));
+        if (addressIndex === -1) {
+            await this.logger.logError(`Address not found for user ${userId}`);
+            throw ApiError.BadRequest('Address not found');
+        }
+
+        user.addresses.splice(addressIndex, 1);
+        await user.save();
+
+        await this.logger.logInfo(`Address deleted for user ${userId}`);
+        return user;
+    }
+
+    async updatePersonalDetails(id: string | null, email: string | null, name: string | null, surname: string | null, phone_number: string | null, address: string | null, BIO: string | null) {
+        const user = <IUser>(
+            await User.findByIdAndUpdate(id, {
+                name: name,
+                surname: surname,
+                email: email,
+                phone_number: phone_number,
+                address: address,
+                BIO: BIO
+            })
+        );
+        if (!user) {
+            await this.logger.logError(`User not found while updating details for ID: ${id}`);
+            throw ApiError.BadRequest("User not found");
+        }
+        await user.save();
+        await this.logger.logInfo(`Personal details updated for user with ID: ${id}`);
+    }
+}
