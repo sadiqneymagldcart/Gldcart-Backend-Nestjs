0853141824bbeffde2ff27a44f4b01985285d0f7
sappChak <konotop401@gmail.com>
2024-01-25 10:40:41 +0100

    Add subscription middleware
    A new middleware has been added to handle a subscription validation. This middleware checks if a user's subscription is active and matches the required subscription type before proceeding to the next stage of the request handling process. If validation fails, the middleware sends

---
+++ src/middlewares/subscription.middleware.ts
@@ -0,0 +1,41 @@
+import {NextFunction, Request, Response} from "express";
+import {ApiError} from "../exceptions/api.error";
+import User from "../models/User";
+import {Logger} from "../utils/logger";
+import {container} from "../config/inversify.config";
+
+export const requireSubscription = (requiredSubscriptionType: string) => {
+    return async (
+        request: Request,
+        response: Response,
+        next: NextFunction,
+    ): Promise<void> => {
+        try {
+            const userId = response.locals.user.userId;
+
+            if (!userId) {
+                return next(ApiError.UnauthorizedError());
+            }
+
+            const user = await User.findById(userId).populate("activeSubscription");
+
+            if (!user || !user.activeSubscription) {
+                response.status(403).json({error: "No active subscription found"});
+            }
+
+            const userSubscriptionType = user?.activeSubscription;
+
+            if (userSubscriptionType !== requiredSubscriptionType) {
+                response
+                    .status(403)
+                    .json({error: "Access denied for this subscription type"});
+            }
+            return next();
+        } catch (error: any) {
+            await container
+                .get(Logger)
+                .logError("Error checking subscription:", error);
+            response.status(500).json({error: "Internal server error"});
+        }
+    };
+};
