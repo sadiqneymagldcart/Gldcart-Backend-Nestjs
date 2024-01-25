cb34aabb4289344280b5a719c8b92122db4b002f
sappChak <konotop401@gmail.com>
2024-01-25 17:21:53 +0100

    Move User model to separate directory
    Moved User model file from the 'models' directory to a new 'user' subdirectory, creating a more organized file structure. This change promotes better project organization, making the hierarchy easier to understand and the code easier to maintain.

---
+++ src/models/user/User.ts
@@ -0,0 +1,69 @@
+import mongoose, {Document, Model, Schema} from "mongoose";
+import validator from "validator";
+import {addressSchema, IAddress} from "./Address";
+
+export interface IUser extends Document {
+    _id: string;
+    type: string;
+    name: string;
+    surname: string;
+    email: string;
+    addresses: IAddress[];
+    picture: string;
+    password: string;
+    passwordResetToken?: string;
+    activeSubscription: string | null;
+    BIO?: string;
+    phone_number?: string;
+    address?: string;
+}
+
+const userSchema: Schema<IUser> = new Schema({
+    type: {
+        type: String,
+        required: [true, "Client's type is undefined"],
+    },
+    name: {
+        type: String,
+        required: [true, "Please, enter your first name"],
+    },
+    surname: {
+        type: String,
+    },
+    email: {
+        type: String,
+        required: [true, "Please, enter an contact"],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, "Please, enter a valid contact"],
+    },
+    addresses: [addressSchema],
+    picture: {
+        type: String,
+    },
+    password: {
+        type: String,
+        required: [true, "Please, enter a password"],
+        minlength: [6, "Minimum password length is 6 characters"],
+    },
+    passwordResetToken: {
+        type: String,
+    },
+    activeSubscription: {
+        type: String,
+        ref: 'Subscription',
+        default: null,
+    },
+    BIO: {
+        type: String,
+    },
+    phone_number: {
+        type: String,
+    },
+    address: {
+        type: String,
+    },
+});
+
+const User: Model<IUser> = mongoose.model("User", userSchema) as Model<IUser>;
+export default User;
