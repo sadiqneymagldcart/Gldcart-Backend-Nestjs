a5d042e285c91fac108408de809cf064a6d2e1df
sappChak <konotop401@gmail.com>
2024-01-25 17:22:55 +0100

    Refactor: Relocate Token model to user directory
    The Refactor involved moving the Token model from the main 'models' directory to the 'user' subdirectory. This commit was made to enhance the organization of model files, thereby improving code manageability and intuitiveness.

--- src/models/user/Token.ts
+++ src/models/user/Token.ts
@@ -1,4 +1,4 @@
-import mongoose, { Document, Model, Schema } from 'mongoose';
+import mongoose, {Document, Model, Schema} from 'mongoose';
 
 export interface IToken extends Document {
     user: Schema.Types.ObjectId;
