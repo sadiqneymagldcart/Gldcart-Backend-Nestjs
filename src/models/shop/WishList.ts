64e4903b38b3c8c51a1e2557e22b9be2659cd29b
sappChak <konotop401@gmail.com>
2024-01-25 17:22:26 +0100

    Update WishList import statement after move to shop subdirectory
    The import statement in the WishList model has been updated to reflect its relocation. The model file was moved from the 'models' directory to a new 'shop' subdirectory. This adjustment supports code clarity and maintainability in

--- src/models/shop/WishList.ts
+++ src/models/shop/WishList.ts
@@ -1,4 +1,5 @@
-import mongoose, { Document, Schema } from 'mongoose';
+import mongoose, {Document, Schema} from 'mongoose';
+
 interface IWishlist extends Document {
     user: mongoose.Types.ObjectId;
     products: mongoose.Types.ObjectId[];
