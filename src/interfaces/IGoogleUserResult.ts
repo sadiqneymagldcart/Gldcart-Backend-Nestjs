b9d8820c27a16c38b85ef2b0c130476d6e2073a9
sappChak <konotop401@gmail.com>
2024-01-25 10:36:37 +0100

    Reworked auth interfaces and renamed GoogleUserResult
    The file 'GoogleUserResult.ts' has been renamed to 'IGoogleUserResult.ts' and its interface has been updated to include a 'link' field. Two new interfaces, 'IOAuthValues' and 'IGoogleUserinfo', have been added to provide structured handling of OAuth values and Google user information, respectively. The key field in 'ITokenPayload' was also renamed to '_id'.

--- src/interfaces/IGoogleUserResult.ts
+++ src/interfaces/IGoogleUserResult.ts
@@ -1,10 +1,11 @@
-export interface GoogleUserResult {
+export interface IGoogleUserResult {
     id: string;
     email: string;
     verified_email: boolean;
     name: string;
     given_name: string;
     family_name: string;
+    link: string;
     picture: string;
     locale: string;
 }
