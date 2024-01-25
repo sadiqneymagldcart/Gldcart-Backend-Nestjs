e3386d78d1a94da9d8d57622254867b4a00c3518
sappChak <konotop401@gmail.com>
2024-01-25 15:26:12 +0100

    Remove type annotation for addressSchema
    The type annotation for `addressSchema` in the Address model has been removed. While this was previously specifically marked as a Schema of IAddress, such explicit typing was deemed unnecessary and thus it has been simplified for cleaner code.

---
+++ src/models/Address.ts
@@ -0,0 +1,43 @@
+import {Schema, Types} from "mongoose";
+
+export interface IAddress
+{
+    id?: Types.ObjectId;
+    recipients_name: string;
+    street_address: string;
+    city: string;
+    state?: string;
+    country: string;
+    ZIP_code: string;
+    phone_number: string;
+}
+
+export const addressSchema = new Schema<IAddress>({
+    recipients_name: {
+        type: String,
+        required: true
+    },
+    street_address: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: String
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    ZIP_code: {
+        type: String,
+        required: true
+    },
+    phone_number: {
+        type: String,
+        required: true
+    }
+});
