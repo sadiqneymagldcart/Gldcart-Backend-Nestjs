7aa7d2caa593cd37c5847f89792497787b385417
sappChak <konotop401@gmail.com>
2024-01-25 13:44:51 +0100

    Add Stripe Payment Controller
    This commit introduces a new Payment Controller for Stripe. The controller handles two key operations: creation of a

---
+++ src/controllers/stripe/payment.controller.ts
@@ -0,0 +1,46 @@
+import {NextFunction, Request, Response} from "express";
+import {PaymentService} from "../../services/stripe/payment.service";
+import {inject} from "inversify";
+import {controller, httpPost} from "inversify-express-utils";
+import {ICheckoutRequestBody} from "../../interfaces/ICheckoutRequestBody";
+
+@controller("/payment")
+export class PaymentController {
+    private readonly paymentService: PaymentService;
+
+    constructor(@inject(PaymentService) paymentService: PaymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @httpPost("/create-customer")
+    public async createCustomer(
+        request: Request,
+        response: Response,
+        next: NextFunction,
+    ): Promise<void> {
+        const {email, name} = request.body;
+        try {
+            const customerId = await this.paymentService.createCustomer(email, name);
+            response.send(customerId);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    @httpPost("/create-payment-intent")
+    public async createPaymentCheckout(
+        request: Request,
+        response: Response,
+        next: NextFunction,
+    ): Promise<void> {
+        const requestBody = request.body as ICheckoutRequestBody;
+        try {
+            const checkoutUrl = await this.paymentService.createPaymentCheckout(
+                requestBody,
+            );
+            response.json({url: checkoutUrl});
+        } catch (error) {
+            next(error);
+        }
+    }
+}
