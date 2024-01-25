6f44dfb6f6bd3b00ad16959e4948a1790e38bd88
sappChak <konotop401@gmail.com>
2024-01-25 10:41:25 +0100

    Remove 'tags.ts' and 'types.ts' files
    The 'tags.ts' and 'types.ts' files from the src/constant directory have been deleted as they are no longer required. The system dependencies have been revised to ensure seamless execution after the deletion

--- src/constant/types.ts
+++
@@ -1,8 +0,0 @@
-const TYPES = {
-    AuthService: Symbol.for("AuthService"),
-    TokenService: Symbol.for("TokenService"),
-    JwtService: Symbol.for("JwtService"),
-    LoggerService: Symbol.for("LoggerService"),
-};
-
-export { TYPES };
