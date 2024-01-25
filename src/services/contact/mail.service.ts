2fbe25402810072221912d5772ce1e176d2153f9
sappChak <konotop401@gmail.com>
2024-01-25 13:44:21 +0100

    Refactor MailService class for dependency injection
    The MailService class was refactored to use dependency injection for better testability and modularity. The transporter object is now injected into the constructor instead of being created in it. This allows functionality like sending emails or resetting passwords to be more easily unit tested and swapped out for different implementations.

--- src/services/contact/mail.service.ts
+++ src/services/contact/mail.service.ts
@@ -1,23 +1,27 @@
-import nodemailer from "nodemailer";
+import {Transporter} from "nodemailer";
 import {ApiError} from "../../exceptions/api.error";
 import {Logger} from "../../utils/logger";
+import {inject, injectable} from "inversify";
 
+@injectable()
 export class MailService {
-    private transporter: nodemailer.Transporter;
+    private transporter: Transporter;
     private logger: Logger;
 
-    constructor(logger: Logger) {
+    constructor(
+        @inject(Logger) logger: Logger,
+        @inject("NodemailerTransporter") transporter: Transporter,
+    ) {
         this.logger = logger;
-        this.transporter = nodemailer.createTransport({
-            service: "gmail",
-            auth: {
-                user: process.env.SMTP_USER as string,
-                pass: process.env.SMTP_USER_PASSWORD as string,
-            },
-        });
+        this.transporter = transporter;
     }
 
-    async sendContactMail(name: string, email: string, subject: string, message: string) {
+    public async sendContactMail(
+        name: string,
+        email: string,
+        subject: string,
+        message: string,
+    ) {
         try {
             await this.transporter.sendMail({
                 from: "GLDCart Feedback",
@@ -25,14 +29,16 @@ export class MailService {
                 subject: subject,
                 text: `My name is: ${name}. My email is: ${email}. ${message}`,
             });
-            await this.logger.logInfo(`Email was sent to ${process.env.FEEDBACK_EMAIL}`);
+            await this.logger.logInfo(
+                `Email was sent to ${process.env.FEEDBACK_EMAIL}`,
+            );
         } catch (error: any) {
-            await this.logger.logError('Failed to send contact contact', error);
+            await this.logger.logError("Failed to send contact contact", error);
             throw ApiError.BadRequest("Failed to send contact");
         }
     }
 
-    async sendResetPasswordMail(email: string, link: string) {
+    public async sendResetPasswordMail(email: string, link: string) {
         await this.transporter.sendMail({
             from: "GLDCart",
             to: email,
@@ -53,6 +59,6 @@ export class MailService {
            </html>
 `,
         });
-        await this.logger.logInfo('Reset password contact was sent');
+        await this.logger.logInfo("Reset password contact was sent");
     }
 }
