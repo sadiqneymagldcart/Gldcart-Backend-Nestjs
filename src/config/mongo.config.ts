ab4af0bbd2e21048ffa07a5d93bfdd48eeab48cc
sappChak <konotop401@gmail.com>
2024-01-25 13:44:30 +0100

    Remove MongoConfig interface and constant from mongo.config
    The MongoConfig interface and its associated constant have been removed from mongo.config.ts file. This was done to simplify the code and remove unnecessary database configurations, which are now handled in separate files with a more streamlined approach.

--- src/config/mongo.config.ts
+++ src/config/mongo.config.ts
@@ -1,15 +1,5 @@
 import {ConnectOptions} from "mongoose";
 
-interface MongoConfig {
-    DB_URL?: string;
-    DB_PORT: number | string;
-}
-
-const mongoConfig: MongoConfig = {
-    DB_URL: process.env.DB_URL,
-    DB_PORT: process.env.DB_PORT || 27017,
-};
-
 export const mongooseOptions = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
