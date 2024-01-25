e776c1972ee7e4ee0c2b22be7ae42efdd9aa3a37
sappChak <konotop401@gmail.com>
2024-01-25 10:18:39 +0100

    Remove unnecessary files

--- dist/models/WishList.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose_1 = require("mongoose");
-var wishlistSchema = new mongoose_1.Schema({
-    user: { type: mongoose_1.Schema.Types.ObjectId, ref: 'User', required: true },
-    products: [{ type: mongoose_1.Schema.Types.ObjectId, ref: 'Product' }]
-});
-var WishList = mongoose_1.default.model('Wishlist', wishlistSchema);
-exports.default = WishList;
