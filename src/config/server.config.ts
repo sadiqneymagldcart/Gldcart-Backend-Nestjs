20bd575ecef75ce939e15edc833769c90c481dae
sappChak <konotop401@gmail.com>
2024-01-25 10:40:29 +0100

    Update server configuration imports and middleware
    The server configuration has been updated with changes to import and middleware statements. Express and several middleware have been shifted to wildcard imports, while bodyParser has been added to handle raw webhook data. These changes aim to streamline the server configuration and improve request handling.

--- src/config/server.config.ts
+++ src/config/server.config.ts
@@ -1,18 +1,28 @@
-import compression from "compression";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import express, { Application } from "express";
+import * as express from "express";
+import {Application} from "express";
+import {errorHandler} from "../middlewares/error.middleware";
+import * as compression from "compression";
+import * as cookieParser from "cookie-parser";
+import * as cors from "cors";
+import * as hpp from "hpp";
+import * as bodyParser from "body-parser";
 import helmet from "helmet";
-import hpp from "hpp";
-import { errorHandler } from "../middlewares/errorMiddleware";
 
 export async function serverConfig(app: Application) {
     app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+
     app.use(hpp());
+
     app.use(helmet());
+
     app.use(compression());
+
+    app.use("/webhook", bodyParser.raw({type: "*/*"}));
+
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
+
     app.use(cookieParser());
+
     app.use(errorHandler);
 }
