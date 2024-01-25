33fe6e3af98fb2e4c38bf1d04912fdc7fc773419
sappChak <konotop401@gmail.com>
2024-01-25 13:45:15 +0100

    Refactor Stripe Subscription Service for IoC
    The Stripe Subscription Service has been revised to use Inversion of Control (IoC) through Dependency Injection. This modification primarily

--- src/services/stripe/stripe.subscription.service.ts
+++ src/services/stripe/stripe.subscription.service.ts
@@ -1,16 +1,21 @@
 import {Logger} from "../../utils/logger";
 import Stripe from "stripe";
 import {BaseService} from "../base.service";
+import {inject, injectable} from "inversify";
 
-export class StripeSubscriptionService extends BaseService{
+@injectable()
+export class StripeSubscriptionService extends BaseService {
     private stripe: Stripe;
 
-    constructor(logger: Logger, stripe: Stripe) {
+    constructor(@inject(Logger) logger: Logger, @inject(Stripe) stripe: Stripe) {
         super(logger);
         this.stripe = stripe;
     }
 
-    async createSubscriptionCheckout(userId: string, lookupKey: string): Promise<string | null> {
+    async createSubscriptionCheckout(
+        userId: string,
+        lookupKey: string,
+    ): Promise<string | null> {
         try {
             await this.stripe.customers.create({
                 metadata: {
@@ -20,41 +25,46 @@ export class StripeSubscriptionService extends BaseService{
 
             const prices = await this.stripe.prices.list({
                 lookup_keys: [lookupKey],
-                expand: ['data.product'],
+                expand: ["data.product"],
             });
 
             const session = await this.stripe.checkout.sessions.create({
-                billing_address_collection: 'auto',
+                billing_address_collection: "auto",
                 line_items: [
                     {
                         price: prices.data[0].id,
                         quantity: 1,
                     },
                 ],
-                mode: 'subscription',
+                mode: "subscription",
                 success_url: `${process.env.CLIENT_URL}/?success=true&session_id={CHECKOUT_SESSION_ID}`,
                 cancel_url: `${process.env.CLIENT_URL}?canceled=true`,
             });
 
-            await this.logger.logInfo(`Subscription checkout session created: ${session.id}`);
+            await this.logger.logInfo(
+                `Subscription checkout session created: ${session.id}`,
+            );
 
             return session.url;
         } catch (error: any) {
-            await this.logger.logError('Failed to create subscription checkout session', error);
+            await this.logger.logError(
+                "Failed to create subscription checkout session",
+                error,
+            );
             throw error;
         }
     }
 
     async cancelSubscription(subscriptionId: string): Promise<any> {
         try {
-
-            const canceledSubscription = await this.stripe.subscriptions.cancel(subscriptionId);
+            const canceledSubscription =
+                await this.stripe.subscriptions.cancel(subscriptionId);
 
             await this.logger.logInfo(`Subscription cancelled: ${subscriptionId}`);
 
             return canceledSubscription;
         } catch (error: any) {
-            await this.logger.logError('Failed to cancel subscription', error);
+            await this.logger.logError("Failed to cancel subscription", error);
             throw error;
         }
     }
