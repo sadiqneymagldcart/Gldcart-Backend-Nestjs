dc1e7d938c8c8f3cc90204f27b64813155648ccd
sappChak <konotop401@gmail.com>
2024-01-26 14:08:17 +0100

    Refactor Stripe Payment service code
    The payment service has undergone significant changes to increase readability and maintainability. Functions have been split into smaller, more specific tasks. The import of IProduct has also been corrected, pointing its location to the shop folder. Logging has been streamlined removing unnecessary async calls, enhancing application performance.

--- src/services/stripe/payment.service.ts
+++ src/services/stripe/payment.service.ts
@@ -1,6 +1,6 @@
 import {Logger} from "../../utils/logger";
 import Stripe from "stripe";
-import {IProduct} from "../../models/Product";
+import {IProduct} from "../../models/shop/Product";
 import {BaseService} from "../base.service";
 import {ICheckoutRequestBody} from "../../interfaces/ICheckoutRequestBody";
 import {inject, injectable} from "inversify";
@@ -16,85 +16,64 @@ export class PaymentService extends BaseService {
 
     public async createCustomer(email: string, name: string): Promise<string> {
         try {
-            const customer = await this.stripe.customers.create({
-                email: `${email}`,
-                name: `${name}`,
-                shipping: {
-                    address: {
-                        city: "Brothers",
-                        country: "US",
-                        line1: "27 Fredrick Ave",
-                        postal_code: "97712",
-                        state: "CA",
-                    },
-                    name: `${name}`,
-                },
-                address: {
-                    city: "Brothers",
-                    country: "US",
-                    line1: "27 Fredrick Ave",
-                    postal_code: "97712",
-                    state: "CA",
-                },
-            });
-
-            await this.logger.logInfo(`Customer created: ${customer.id}`);
-
+            const customer = await this.createCustomerWithMetadata();
+            this.logger.logInfo(`Customer created: ${customer.id}`);
             return customer.id;
         } catch (error: any) {
-            await this.logger.logError("Failed to create customer", error);
+            this.logger.logError("Failed to create customer", error);
             throw error;
         }
     }
 
-    public async createPaymentCheckout(
-        requestBody: ICheckoutRequestBody,
-    ): Promise<string | null> {
+    public async createPaymentCheckout(requestBody: ICheckoutRequestBody): Promise<string | null> {
         try {
-            const customer = await this.stripe.customers.create({
-                metadata: {
-                    userId: requestBody.userId,
-                    cart: JSON.stringify(requestBody.cartItems),
-                },
+            const customer = await this.createCustomerWithMetadata({
+                userId: requestBody.userId,
+                cart: JSON.stringify(requestBody.cartItems),
             });
-
-            const line_items = requestBody.cartItems.map((item: IProduct) => ({
-                price_data: {
-                    currency: "usd",
-                    product_data: {
-                        name: item.title,
-                        images: [item.imageURL],
-                        description: item.description,
-                        // metadata: {
-                        //     id: item.productId,
-                        // },
-                    },
-                    unit_amount: item.price * 100,
-                },
-                quantity: item.quantity,
-            }));
-
-            const session = await this.stripe.checkout.sessions.create({
-                payment_method_types: ["card"],
-                shipping_address_collection: {
-                    allowed_countries: ["US", "UA", "SK"],
-                },
-                phone_number_collection: {
-                    enabled: true,
-                },
-                customer: customer.id,
-                line_items,
-                mode: "payment",
-                success_url: `${process.env.CLIENT_URL}/checkout-success`,
-                cancel_url: `${process.env.CLIENT_URL}/checkout-failed`,
-            });
-
-            await this.logger.logInfo(`Checkout session created: ${session.id}`);
-
+            const lineItems = this.createLineItems(requestBody.cartItems);
+            const session = await this.createCheckoutSession(customer.id, lineItems);
+            this.logger.logInfo(`Checkout session created: ${session.id}`);
             return session.url;
         } catch (error: any) {
-            await this.logger.logError("Failed to create checkout session", error);
+            this.logger.logError("Failed to create checkout session", error);
             throw error;
         }
+    }
+
+    private async createCustomerWithMetadata(metadata: Record<string, any> = {}): Promise<Stripe.Customer> {
+        return this.stripe.customers.create({metadata});
+    }
+
+    private createLineItems(cartItems: IProduct[]): Array<Object> {
+        return cartItems.map(item => ({
+            price_data: {
+                currency: "usd",
+                product_data: {
+                    name: item.name,
+                    images: [item.images[0]],
+                    description: item.short_description,
+                },
+                unit_amount: item.price * 100,
+            },
+            quantity: item.quantity,
+        }));
+    }
+
+    private async createCheckoutSession(customerId: string, lineItems: Object[]): Promise<Stripe.Checkout.Session> {
+        return this.stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            shipping_address_collection: {
+                allowed_countries: ["US", "UA", "SK"],
+            },
+            phone_number_collection: {
+                enabled: true,
+            },
+            customer: customerId,
+            line_items: lineItems,
+            mode: "payment",
+            success_url: `${process.env.CLIENT_URL}/checkout-success`,
+            cancel_url: `${process.env.CLIENT_URL}/checkout-failed`,
+        });
     }
 }
