f5f9e99ada39aef47705f280d4334d62b511cac7
sappChak <konotop401@gmail.com>
2024-01-26 21:37:35 +0100

    Change import style in jwt.service.ts
    Updated how 'jsonwebtoken' module is imported in jwt.service.ts for consistency. Previously, it was simply imported, but it's now imported as 'jwt'. This enhances code uniformity with other parts of the application where similar naming conventions are followed.

--- src/services/token/jwt.service.ts
+++ src/services/token/jwt.service.ts
@@ -1,4 +1,4 @@
-import jwt from "jsonwebtoken";
+import * as jwt from "jsonwebtoken";
 import {BaseService} from "../base.service";
 import {Logger} from "../../utils/logger";
 import {inject, injectable} from "inversify";
