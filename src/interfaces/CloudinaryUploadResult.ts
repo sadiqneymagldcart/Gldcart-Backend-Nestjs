e8c576aa875db13bacc85ec784436c951de59707
sappChak <konotop401@gmail.com>
2024-02-06 09:32:20 +0100

    Extract to interfaces folder

---
+++ src/interfaces/CloudinaryUploadResult.ts
@@ -0,0 +1,16 @@
+export interface CloudinaryUploadResult {
+    public_id: string;
+    version: number;
+    signature: string;
+    width: number;
+    height: number;
+    format: string;
+    created_at: string;
+    bytes: number;
+    etag: string;
+    placeholder: boolean;
+    url: string;
+    secure_url: string;
+    original_filename: string;
+}
+
