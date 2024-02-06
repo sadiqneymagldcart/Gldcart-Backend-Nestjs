59fafecfc5c6122098bb98874f44d2d9d214609d
sappChak <konotop401@gmail.com>
2024-02-06 13:03:48 +0100

    Commit 96

--- src/utils/cloudinary.util.ts
+++ src/utils/cloudinary.util.ts
@@ -1,5 +1,6 @@
-import * as cloudinary from 'cloudinary'
-import {configDotenv} from "dotenv";
+import * as cloudinary from "cloudinary";
+import { configDotenv } from "dotenv";
+import { CloudinaryUploadResult } from "../interfaces/CloudinaryUploadResult";
 
 configDotenv();
 
@@ -9,31 +10,20 @@ cloudinary.v2.config({
     api_secret: process.env.CLOUD_API_SECRET,
 });
 
-interface CloudinaryUploadResult {
-    public_id: string;
-    version: number;
-    signature: string;
-    width: number;
-    height: number;
-    format: string;
-    created_at: string;
-    bytes: number;
-    etag: string;
-    placeholder: boolean;
-    url: string;
-    secure_url: string;
-    original_filename: string;
-}
-
-export const uploadToCloudinary: (file: Express.Multer.File) => Promise<CloudinaryUploadResult> = (file: Express.Multer.File) => {
+export const uploadToCloudinary: (
+    file: Express.Multer.File,
+) => Promise<CloudinaryUploadResult> = (file: Express.Multer.File) => {
     return new Promise((resolve, reject) => {
-        const uploadStream = cloudinary.v2.uploader.upload_stream({resource_type: 'auto'}, (error, result) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(result);
-            }
-        });
+        const uploadStream = cloudinary.v2.uploader.upload_stream(
+            { resource_type: "auto" },
+            (error, result) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(result);
+                }
+            },
+        );
 
         uploadStream.end(file.buffer);
     });
