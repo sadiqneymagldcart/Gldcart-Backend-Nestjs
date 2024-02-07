276c08f05fbc128d48b4807be1e8bfcc804dc4e5
sappChak <konotop401@gmail.com>
2024-02-07 10:51:52 +0100

    Send secure url, instead of unsecure

--- src/services/shop/image.service.ts
+++ src/services/shop/image.service.ts
@@ -13,8 +13,8 @@ export class ImageService extends BaseService {
         return await Promise.all(
             files.map(async (file) => {
                 const result = await uploadToCloudinary(file);
-                console.log(result.url);
-                return result.url;
+                console.log(result.secure_url);
+                return result.secure_url;
             }),
         );
     }
