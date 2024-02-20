e8c2eedd105f215cd2ee9d4e3cb284d0f3c94a40
sappChak <konotop401@gmail.com>
2024-02-20 14:30:48 +0100

    Implement global search functionality

---
+++ src/controllers/shop/global.search.controller.ts
@@ -0,0 +1,28 @@
+import * as express from "express";
+import { inject } from "inversify";
+import { SearchService } from "../../services/shop/global.search.service";
+import { controller, httpGet } from "inversify-express-utils";
+
+@controller("/search")
+export class SearchController {
+    private readonly searchService: SearchService;
+    public constructor(@inject(SearchService) searchService: SearchService) {
+        this.searchService = searchService;
+    }
+
+    @httpGet("/")
+    public async searchGlobally(
+        request: express.Request,
+        response: express.Response,
+        next: express.NextFunction,
+    ) {
+        try {
+            const query = request.query.query as string;
+            const result =
+                await this.searchService.searchGloballyByLetter(query);
+            response.status(200).json(result);
+        } catch (error) {
+            next(error);
+        }
+    }
+}
