50d9dc787388aea48f971c9b3ed195407ffb60d9
sappChak <konotop401@gmail.com>
2024-03-10 20:05:46 +0100

    Allow sending files

--- src/socket.ts
+++ src/socket.ts
@@ -1,8 +1,9 @@
-import { Server, Socket } from "socket.io";
+import { Socket } from "socket.io";
 import { ChatModel } from "./models/chat/Chat";
 import { Message, MessageModel } from "./models/chat/Message";
+import { AwsStorage } from "./storages/aws.storage";
 
-export async function createSocket(io: Server) {
+export async function createSocket(io: any, awsStorage: AwsStorage) {
     io.on("connection", (socket: Socket) => {
         socket.on("join", async (chatId: string) => {
             try {
@@ -16,9 +17,13 @@ export async function createSocket(io: Server) {
                 socket.emit("error", { message: error.message });
             }
         });
-        socket.on("message", async (message: Message) => {
+
+        socket.on("message", async (data: any) => {
             try {
-                if (!message.text || !message.senderId || !message.chatId) {
+                let message: Message;
+                if (data.text) {
+                    message = data;
+                } else {
                     throw new Error("Invalid message data");
                 }
                 const chat = await ChatModel.findById(message.chatId);
@@ -31,11 +36,34 @@ export async function createSocket(io: Server) {
                 socket.emit("error", { message: error.message });
             }
         });
+
+        socket.on("file", async (data: any) => {
+            try {
+                const urls = await awsStorage.upload([data.file]);
+                const message: Message = {
+                    chatId: data.chatId,
+                    senderId: data.senderId,
+                    text: `${urls[0]}`, 
+                    recipientId: data.recipientId,
+                };
+                const chat = await ChatModel.findById(message.chatId);
+                if (!chat) {
+                    throw new Error("Chat not found");
+                }
+                const savedMessage = await MessageModel.create(message);
+                io.to(message.chatId).emit("message", savedMessage);
+            } catch (error) {
+                socket.emit("error", { message: error.message });
+            }
+        });
+
         socket.on("leave", (chatId: string) => {
             socket.leave(chatId);
         });
+
         socket.on("disconnect", () => {
             console.log("user disconnected");
         });
     });
 }
+
