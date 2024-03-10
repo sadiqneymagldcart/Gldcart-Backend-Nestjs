211ce2f65512c4325ede76ef09b43b7b70ebad38
sappChak <konotop401@gmail.com>
2024-03-10 20:11:37 +0100

    Fix importing Model

--- src/services/chat/chat.service.ts
+++ src/services/chat/chat.service.ts
@@ -1,26 +1,19 @@
 import { inject, injectable } from "inversify";
 import { Chat, ChatModel } from "../../models/chat/Chat";
-import { Model } from "mongoose";
 
 @injectable()
 export class ChatService {
-    private readonly chatModel: Model<Chat>;
-
-    public constructor(@inject(ChatModel) chatModel: Model<Chat>) {
-        this.chatModel = chatModel;
-    }
-
     public async getChats(userId: string): Promise<Chat[]> {
-        return this.chatModel.find({ participants: userId });
+        return ChatModel.find({ participants: userId });
     }
 
     public async createChat(participants: string[]): Promise<Chat> {
-        const newChat = new this.chatModel({ participants });
+        const newChat = new ChatModel({ participants });
         return newChat.save();
     }
 
     public async getChatMessages(chatId: string): Promise<Chat | null> {
-        const chat = await this.chatModel.findById(chatId).populate("messages");
+        const chat = await ChatModel.findById(chatId).populate("messages");
         return chat;
     }
 }
