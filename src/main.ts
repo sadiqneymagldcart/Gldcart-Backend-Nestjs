a96334f3235a4aa8f977f682e81151b9f6a3d7bc
sappChak <konotop401@gmail.com>
2024-03-11 20:16:17 +0100

    Refactor socket implementation

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import { App } from "./app";
 import { InversifyExpressServer } from "inversify-express-utils";
 import { Logger } from "./utils/logger";
 import { container } from "./config/inversify.config";
+import { AwsStorage } from "./storages/aws.storage";
+import { CustomSocket } from "./socket";
 
 const server = new InversifyExpressServer(container);
-const app = new App(server, new Logger());
+const app = new App(server, new Logger(), 3001);
 app.start();
 
+const socket = new CustomSocket(new AwsStorage(), new Logger(), 3002);
+socket.start();
 
