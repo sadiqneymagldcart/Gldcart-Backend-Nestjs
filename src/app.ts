b2248fc8b9f2f31649a49d24f33f9420ef0a69ec
sappChak <konotop401@gmail.com>
2024-03-11 20:16:17 +0100

    Refactor socket implementation

--- src/app.ts
+++ src/app.ts
@@ -5,36 +5,28 @@ import { mongooseOptions } from "./config/mongo.config";
 import { serverConfig } from "./config/server.config";
 import { errorHandlerMiddleware } from "./middlewares/error.middleware";
 import { loadEnvironmentVariables } from "./config/env.config";
-import { Server } from "socket.io";
-import * as http from "http";
-import { createSocket } from "./socket";
 
 export class App {
     private readonly port: string | number;
     private readonly logger: Logger;
     private readonly server: InversifyExpressServer;
-    private readonly httpServer: http.Server;
-    private readonly io: Server;
 
-    public constructor(server: InversifyExpressServer, logger: Logger) {
+    public constructor(
+        server: InversifyExpressServer,
+        logger: Logger,
+        port: number,
+    ) {
         loadEnvironmentVariables();
         this.validateEnvironmentVariables();
-        this.port = process.env.PORT || 3000;
+        this.port = port;
         this.server = server;
         this.logger = logger;
-        this.httpServer = http.createServer();
-        this.io = new Server(this.httpServer, {
-            cors: {
-                origin: "*",
-            },
-        });
     }
 
     public async start() {
         try {
             await this.connectToDatabase();
             this.setupServer();
-            this.setupSocket();
         } catch (error) {
             this.logger.logError("Server startup error: " + error.message, error);
             process.exit(1);
@@ -67,14 +59,5 @@ export class App {
                 `⚡️[server]: Server is running at ${process.env.HOST}:${this.port} in ${process.env.NODE_ENV} mode`,
             ),
         );
-    }
-
-    private setupSocket() {
-        this.httpServer.listen(3002, () => {
-            this.logger.logInfo(
-                `⚡️[socket]: Socket server is running on address ${JSON.stringify(this.httpServer.address())}`,
-            );
-        });
-        createSocket(this.io);
     }
 }
