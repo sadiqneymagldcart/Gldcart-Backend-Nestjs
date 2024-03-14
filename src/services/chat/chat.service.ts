b7487f131db5c7a08c0e539404f82cb58987bbec
sappChak <konotop401@gmail.com>
2024-03-14 20:44:05 +0100

    Return only necessery fields

--- src/services/chat/chat.service.ts
+++ src/services/chat/chat.service.ts
@@ -3,17 +3,22 @@ import { Chat, ChatModel } from "../../models/chat/Chat";
 
 @injectable()
 export class ChatService {
-    public async getChats(userId: string): Promise<Chat[]> {
-        return ChatModel.find({ participants: userId });
-    }
+  public async getChats(userId: string): Promise<Chat[]> {
+    return ChatModel.find({ participants: userId }).populate({
+      path: "participants",
+      select: { name: 1, surname: 1, type: 1, is_online: 1 },
+    });
+  }
 
-    public async createChat(participants: string[]): Promise<Chat> {
-        const newChat = new ChatModel({ participants });
-        return newChat.save();
-    }
+  public async createChat(participants: string[]): Promise<Chat> {
+    const chat = await ChatModel.findOne({
+      participants: { $all: participants },
+    });
+    if (chat) return chat;
+    return await ChatModel.create({ participants });
+  }
 
-    public async getChatMessages(chatId: string): Promise<Chat | null> {
-        const chat = await ChatModel.findById(chatId).populate("messages");
-        return chat;
-    }
+  public async getChatById(chatId: string): Promise<Chat | null> {
+    return ChatModel.findById(chatId);
+  }
 }
