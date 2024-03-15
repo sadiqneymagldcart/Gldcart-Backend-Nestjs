6f3ba728f48b785d6dcddb7d7569158107745f7f
sappChak <konotop401@gmail.com>
2024-03-15 08:59:26 +0100

    Remove unnecessery messages field

--- src/models/chat/Chat.ts
+++ src/models/chat/Chat.ts
@@ -2,15 +2,15 @@ import mongoose, { Model, Schema } from "mongoose";
 
 export interface Chat extends Document {
     participants: string[];
-    messages: string[];
 }
 
 const chatSchema = new Schema<Chat>(
     {
         participants: [{ type: Schema.Types.ObjectId, ref: "User" }],
-        messages: [{ type: Schema.Types.ObjectId, ref: "Message" }],
     },
     { timestamps: true },
 );
+
+chatSchema.index({ participants: 1 });
 
 export const ChatModel = mongoose.model("Chat", chatSchema) as Model<Chat>;
