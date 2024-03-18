15aeaaa2d4069336a28e3125f6700c2fbb6dbc35
sappChak <konotop401@gmail.com>
2024-03-18 14:49:43 +0100

    Commit 96

--- src/config/stripe.config.ts
+++ src/config/stripe.config.ts
@@ -1,6 +1,7 @@
 import Stripe from "stripe";
 
-export const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY as string;
+export const STRIPE_SECRET_KEY =
+    "sk_test_51LwMMSIr9qomMnpIgv956EjoyG9MPMpJVD0baydsnLLoIitrdjdCvyYhlzecEvE9k8qR1FtAR75ffZ111P8tQILZ008AuRZGJp";
 const STRIPE_API_VERSION = "2023-08-16";
 
 export const stripeConfig: Stripe.StripeConfig = {
