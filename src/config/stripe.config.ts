764e228d6414983327ca8ea17f5816bbe729a51f
sappChak <konotop401@gmail.com>
2024-03-21 00:32:26 +0100

    Remove console.log

--- src/config/stripe.config.ts
+++ src/config/stripe.config.ts
@@ -3,7 +3,6 @@ import { config } from "dotenv";
 config();
 
 export const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
-console.log("Stripe secret key: ", STRIPE_SECRET_KEY);
 const STRIPE_API_VERSION = "2023-08-16";
 
 export const stripeConfig: Stripe.StripeConfig = {
