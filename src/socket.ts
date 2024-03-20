25d8ab51a072c86752cfd502def4678a4932e017
sappChak <konotop401@gmail.com>
2024-03-20 17:45:53 +0100

    Small code improvements

--- src/socket.ts
+++ src/socket.ts
@@ -1,10 +1,11 @@
 import { Server, Socket } from "socket.io";
-import { ChatModel } from "./models/chat/Chat";
-import { Message, MessageModel } from "./models/chat/Message";
-import { AwsStorage } from "./storages/aws.storage";
-import { Logger } from "./utils/logger";
 import * as http from "http";
 import { UserModel } from "./models/user/User";
+import { CustomFile } from "./interfaces/CustomFile";
+import { AwsStorage } from "./storages/aws.storage";
+import { Logger } from "./utils/logger";
+import { ChatModel } from "./models/chat/Chat";
+import { Message, MessageModel } from "./models/chat/Message";
 
 export class CustomSocket {
   private readonly logger: Logger;
@@ -32,11 +33,15 @@ export class CustomSocket {
   private async setupSocketHandlers() {
     this.io.on("connection", async (socket: Socket) => {
       const userId = socket.handshake.query.userId as string;
-      console.log("User connected", userId);
-      await UserModel.updateOne({ _id: userId }, { is_online: true });
+      this.logger.logInfo("User connected", { userId });
+      await this.updateUserOnlineStatus(userId, true);
       this.handleChatsList(socket, userId);
       this.handleConnection(socket);
     });
+  }
+
+  private async updateUserOnlineStatus(userId: string, status: boolean) {
+    await UserModel.updateOne({ _id: userId }, { is_online: status });
   }
 
   private async handleConnection(socket: Socket) {
@@ -44,18 +49,17 @@ export class CustomSocket {
     socket.on("message", (message: Message) =>
       this.handleMessage(socket, message),
     );
-    socket.on("file", (data: any) => this.handleFile(socket, data));
+    socket.on("file", (data: any) => this.handleFiles(socket, data));
     socket.on("leave", (chatId: string) => this.handleLeave(socket, chatId));
     socket.on("disconnect", () => this.handleDisconnect(socket));
   }
 
   private async handleJoin(socket: Socket, chatId: string) {
     try {
-      this.logger.logInfo("User joined chat", { socket: socket.id });
+      this.logger.logInfo("User joined chat", { socketId: socket.id });
       const chat = await ChatModel.findById(chatId);
       if (!chat) throw new Error("Chat not found");
       socket.join(chatId);
-      // this.io.to(chatId).emit("joined", chatId);
     } catch (error) {
       this.handleError(socket, error);
     }
@@ -71,14 +75,13 @@ export class CustomSocket {
       const savedMessage = await MessageModel.create(message);
       socket.to(message.chatId).emit("message", savedMessage);
     } catch (error) {
-      console.error(error);
       this.handleError(socket, error);
     }
   }
 
   private async handleChatsList(socket: Socket, userId: string) {
     try {
-      console.log("Getting chats for user", userId);
+      this.logger.logInfo("Getting chats for user", { userId });
       const chats = await ChatModel.find({ participants: userId }).populate({
         path: "participants",
         select: { name: 1, surname: 1, type: 1, is_online: 1 },
@@ -89,24 +92,27 @@ export class CustomSocket {
     }
   }
 
-  private async handleFile(socket: Socket, data: any) {
+  private async handleFiles(socket: Socket, data: any) {
     try {
-      const urls = await this.uploadFile(data.file);
+      const urls = await this.uploadFiles(data.files);
       const message = {
         chatId: data.chatId,
         senderId: data.senderId,
         text: `${urls[0]}`,
         recipientId: data.recipientId,
       };
-      socket.to(data.chatId).emit("file", message);
-      // await this.saveMessageAndBroadcast(socket, message);
+      const savedMessage = await MessageModel.create(message);
+      socket.to(data.chatId).emit("message", savedMessage);
     } catch (error) {
       this.handleError(socket, error);
     }
   }
 
-  private async uploadFile(file: any): Promise<string[]> {
-    return this.awsStorage.upload([file]);
+  private async uploadFiles(files: CustomFile[]): Promise<string[]> {
+    if (!files || files.length === 0) {
+      throw new Error("No files provided");
+    }
+    return await this.awsStorage.upload(files as Express.Multer.File[]);
   }
 
   private handleLeave(socket: Socket, chatId: string) {
@@ -114,20 +120,14 @@ export class CustomSocket {
   }
 
   private async handleDisconnect(socket: Socket) {
-    const userId = socket.handshake.query.userId;
-    console.log("User disconnected", userId);
-    await UserModel.updateOne({ _id: userId }, { is_online: false });
+    const userId = socket.handshake.query.userId as string;
+    this.logger.logInfo("User disconnected", { userId });
+    await this.updateUserOnlineStatus(userId, false);
   }
 
   private handleError(socket: Socket, error: Error) {
+    this.logger.logError("Error occurred", error);
     socket.emit("error", { message: error.message });
-  }
-
-  private async saveMessageAndBroadcast(socket: Socket, message: any) {
-    const chat = await ChatModel.findById(message.chatId);
-    if (!chat) throw new Error("Chat not found");
-    const savedMessage = await MessageModel.create(message);
-    socket.to(message.chatId).emit("message", savedMessage);
   }
 
   public start() {
