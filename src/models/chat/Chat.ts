75d77f7e2c7c95a389b3296e52e446c276245227
sappChak <konotop401@gmail.com>
2024-03-22 13:34:56 +0100

    Return chats only with messages

--- src/models/chat/Chat.ts
+++ src/models/chat/Chat.ts
@@ -1,12 +1,14 @@
-import mongoose, { Model, Schema } from "mongoose";
+import mongoose, { Document, Model, Schema } from "mongoose";
 
 export interface Chat extends Document {
     participants: string[];
+    messages: string[];
 }
 
 const chatSchema = new Schema<Chat>(
     {
         participants: [{ type: Schema.Types.ObjectId, ref: "User" }],
+        messages: [{ type: Schema.Types.ObjectId, ref: "Message" }],
     },
     { timestamps: true },
 );
