5c669b1cca63a2b35938e76a847b612e11e2713c
sappChak <konotop401@gmail.com>
2024-03-22 16:07:29 +0100

    Disable getting chats on connection

--- src/services/chat/message.service.ts
+++ src/services/chat/message.service.ts
@@ -1,12 +1,30 @@
 import { injectable } from "inversify";
 import { Message, MessageModel } from "../../models/chat/Message";
+import { Chat, ChatModel } from "../../models/chat/Chat";
 
 @injectable()
 export class MessageService {
   public async createMessage(message: Message): Promise<Message> {
-    return await MessageModel.create(message);
-  }
+    let chat: Chat | null = null;
 
+    if (!message.chatId) {
+      chat = await ChatModel.create({
+        participants: [message.senderId, message.recipientId],
+        messages: [],
+      });
+      message.chatId = chat._id;
+    } else {
+      chat = await ChatModel.findById(message.chatId);
+      if (!chat) throw new Error("Chat not found");
+    }
+
+    const savedMessage = await MessageModel.create(message);
+
+    chat.messages.push(savedMessage._id);
+    await chat.save();
+
+    return savedMessage;
+  }
   public async getMessages(chatId: string): Promise<Message[]> {
     return await MessageModel.find({ chatId }).sort({ createdAt: 1 });
   }
