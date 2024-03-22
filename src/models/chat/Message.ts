3b67a9fe3a3873fccab2efdb1816053502bff5ce
sappChak <konotop401@gmail.com>
2024-03-22 13:34:56 +0100

    Return chats only with messages

--- src/models/chat/Message.ts
+++ src/models/chat/Message.ts
@@ -1,4 +1,4 @@
-import mongoose, { Model, Schema } from "mongoose";
+import mongoose, { Document, Model, Schema } from "mongoose";
 
 export interface Message extends Document {
     chatId: string;
