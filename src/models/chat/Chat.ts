c517ad36bbd48e18665fcb5ffdf003c24a3ea173
sappChak <konotop401@gmail.com>
2024-03-29 14:16:25 +0100

    [Bug fix] Remove messages fields

--- src/models/chat/Chat.ts
+++ src/models/chat/Chat.ts
@@ -2,17 +2,17 @@ import mongoose, { Document, Schema } from "mongoose";
 
 export interface Chat extends Document {
     participants: string[];
-    messages: string[];
+    // messages: string[];
 }
 
 const chatSchema = new Schema<Chat>(
     {
         participants: [{ type: Schema.Types.ObjectId, ref: "User" }],
-        messages: [{ type: Schema.Types.ObjectId, ref: "Message" }],
+        // messages: [{ type: Schema.Types.ObjectId, ref: "Message" }],
     },
     { timestamps: true },
 );
 
-chatSchema.index({ participants: 1, messages: 1 });
+chatSchema.index({ participants: 1 });
 
 export const ChatModel = mongoose.model("Chat", chatSchema);
