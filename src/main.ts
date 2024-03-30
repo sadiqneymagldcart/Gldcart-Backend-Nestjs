92b5c1723b7addb6b777175d83e6324cca9f9b1b
sappChak <konotop401@gmail.com>
2024-03-30 18:24:55 +0100

    Remove AWS storage imports

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,13 @@ import { App } from "./app";
 import { InversifyExpressServer } from "inversify-express-utils";
 import { Logger } from "./utils/logger";
 import { container } from "./config/inversify.config";
-import { AwsStorage } from "./storages/aws.storage";
 import { CustomSocket } from "./socket";
 
 const server = new InversifyExpressServer(container);
 
 const logger = container.get<Logger>(Logger);
-const awsStorage = container.get<AwsStorage>(AwsStorage);
 
 const app = new App(3001, logger, server);
 app.start().then(() => {
-    new CustomSocket(awsStorage, logger, app.getHttpServer());
+    new CustomSocket(logger, app.getHttpServer());
 });
