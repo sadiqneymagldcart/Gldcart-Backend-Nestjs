958de8915bc4f2d51a02d5e5f00b20bb755241bf
sappChak <konotop401@gmail.com>
2024-05-05 23:41:51 +0200

    Small changes

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,8 @@ import { Logger } from "@utils/logger";
 import { CustomSocket } from "./socket";
 import { container } from "@/config/inversify.config";
 
-const server = new InversifyExpressServer(container);
-
 const logger = container.get<Logger>(Logger);
-
+const server = new InversifyExpressServer(container);
 const app = new App(3001, logger, server);
 app.start().then(() => {
     new CustomSocket(logger, app.getHttpServer());
