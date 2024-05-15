78ac2307a5f994c18d34601b72de220281acb263
sappChak <konotop401@gmail.com>
2024-05-15 17:51:43 +0200

    Refactor exceptions

---
+++ src/exceptions/internal-server-error.exception.ts
@@ -0,0 +1,16 @@
+import { StatusCodes } from "http-status-codes";
+import * as emoji from "node-emoji";
+
+import { ApiException } from "./api.exception";
+
+class InternalServerErrorException extends ApiException {
+    public constructor() {
+        super(
+            StatusCodes.INTERNAL_SERVER_ERROR,
+            "unexpected_error",
+            `${emoji.get("fire")} An unexpected error has occurred. Please contact the administrator.`,
+        );
+    }
+}
+
+export { InternalServerErrorException };
