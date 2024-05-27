d8c3595ae97c838eb2c599c83701c23e53af1a4b
sappChak <konotop401@gmail.com>
2024-05-27 23:38:47 +0200

    Replace chat socket into separate folder

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import "reflect-metadata";
 import { App } from "./app";
 import { InversifyExpressServer } from "inversify-express-utils";
 import { Logger } from "@utils/logger";
-import { CustomSocket } from "./socket";
 import { container } from "@/config/inversify.config";
+import {ChatSocket} from "@sockets/chat.socket";
 
 const logger = container.get<Logger>(Logger);
 const server = new InversifyExpressServer(container);
 const app = new App(3001, logger, server);
 app.start().then(() => {
-    new CustomSocket(logger, app.getHttpServer());
+    new ChatSocket(logger, app.getHttpServer());
 });
