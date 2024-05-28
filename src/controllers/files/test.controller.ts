646142229725daca54da58d04a018db0dd9fc581
sappChak <konotop401@gmail.com>
2024-05-28 18:01:21 +0200

    Add and inject test service and controller

---
+++ src/controllers/files/test.controller.ts
@@ -0,0 +1,43 @@
+import { inject } from "inversify";
+import {
+    httpGet,
+    controller,
+    request,
+    response,
+    next,
+    Controller,
+    httpPost,
+    BaseHttpController,
+} from "inversify-express-utils";
+import * as express from "express";
+import { TestService } from "@services/base/test.service";
+
+@controller("/test")
+export class TestController extends BaseHttpController implements Controller {
+    private readonly testService: TestService;
+
+    public constructor(@inject(TestService) testService: TestService) {
+        super();
+        this.testService = testService;
+    }
+
+    @httpGet("/")
+    public async index(
+        @request() req: express.Request,
+        @response() res: express.Response,
+        @next() next: express.NextFunction,
+    ): Promise<string> {
+        const id = req.query.id as string;
+        return this.testService.test(id);
+    }
+
+    @httpPost("/")
+    public async createId(
+        @request() req: express.Request,
+        @response() res: express.Response,
+        @next() next: express.NextFunction,
+    ): Promise<string> {
+        const id = req.query.id as string;
+        return this.testService.test(id);
+    }
+}
