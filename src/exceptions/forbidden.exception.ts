f0091585ef21c5d8b5da88402be78d091816d16e
sappChak <konotop401@gmail.com>
2024-05-28 23:02:20 +0200

    Add new kind of exception

---
+++ src/exceptions/forbidden.exception.ts
@@ -0,0 +1,16 @@
+import { StatusCodes } from "http-status-codes";
+import * as emoji from "node-emoji";
+
+import { ApiException } from "./api.exception";
+
+class ForbiddenException extends ApiException {
+  constructor() {
+    super(
+      StatusCodes.FORBIDDEN,
+      "forbidden",
+      `${emoji.get("no_entry_sign")} Forbidden.`,
+    );
+  }
+}
+
+export { ForbiddenException };
