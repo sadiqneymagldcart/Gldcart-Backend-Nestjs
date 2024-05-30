ef2224d2e79285d99e8c19c5832bcfbb47f539a0
sappChak <konotop401@gmail.com>
2024-05-30 13:24:02 +0200

    [refactor] Initialize sockets in App class

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
 import "reflect-metadata";
-import { Server } from "./server";
+import { App } from "./app";
 import { InversifyExpressServer } from "inversify-express-utils";
 import { Logger } from "@utils/logger";
-import { container } from "@/config/inversify.config";
-import { ChatSocket } from "@sockets/chat.socket";
+import { container } from "@config/inversify.config";
 
 const logger = container.get<Logger>(Logger);
 
 const express = new InversifyExpressServer(container);
 
-const server = new Server(3001, logger, express);
-server.start().then(() => {
-    new ChatSocket(logger, server.getConfiguredServer());
-});
+const app = new App(3001, logger, express);
+
+app.start()
