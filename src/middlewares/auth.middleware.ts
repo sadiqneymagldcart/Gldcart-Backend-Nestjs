0ebc2c1b9f601f31d90a07b537095e7e4107d341
sappChak <konotop401@gmail.com>
2024-05-30 12:40:34 +0200

    [refactor] Declare auth middlware as a class

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,46 +1,51 @@
 import * as jwt from "jsonwebtoken";
 import * as express from "express";
 import { UnauthorizedException } from "@exceptions/unauthorized.exception";
+import { injectable } from "inversify";
+import { BaseMiddleware } from "inversify-express-utils";
 
-export const authMiddleware = (
-    request: express.Request,
-    response: express.Response,
-    next: express.NextFunction,
-): void => {
-    const authorizationHeader = request.headers.authorization as string;
+@injectable()
+export class AuthenticationMiddleware extends BaseMiddleware {
+    public handler(
+        request: express.Request,
+        response: express.Response,
+        next: express.NextFunction,
+    ): void {
+        const authorizationHeader = request.headers.authorization as string;
 
-    if (!authorizationHeader) {
-        console.error("No access token was provided");
-        return next(new UnauthorizedException());
-    }
-
-    const parts = authorizationHeader.split(" ");
-    if (parts.length !== 2 || parts[0] !== "Bearer") {
-        console.error("Invalid authorization header format");
-        return next(new UnauthorizedException());
-    }
-
-    const accessToken = parts[1];
-
-    if (!process.env.JWT_ACCESS_SECRET) {
-        console.error("JWT secret is not defined");
-        throw new Error("JWT secret is not defined");
-    }
-
-    try {
-        const userData = jwt.verify(
-            accessToken,
-            process.env.JWT_ACCESS_SECRET,
-        ) as jwt.JwtPayload;
-
-        if (!userData) {
+        if (!authorizationHeader) {
+            console.error("No access token was provided");
             return next(new UnauthorizedException());
         }
 
-        response.locals.user = userData;
-        next();
-    } catch (error) {
-        console.error("Failed to verify access token", error);
-        return next(new UnauthorizedException());
+        const parts = authorizationHeader.split(" ");
+        if (parts.length !== 2 || parts[0] !== "Bearer") {
+            console.error("Invalid authorization header format");
+            return next(new UnauthorizedException());
+        }
+
+        const accessToken = parts[1];
+
+        if (!process.env.JWT_ACCESS_SECRET) {
+            console.error("JWT secret is not defined");
+            throw new Error("JWT secret is not defined");
+        }
+
+        try {
+            const userData = jwt.verify(
+                accessToken,
+                process.env.JWT_ACCESS_SECRET,
+            ) as jwt.JwtPayload;
+
+            if (!userData) {
+                return next(new UnauthorizedException());
+            }
+
+            response.locals.user = userData;
+            next();
+        } catch (error) {
+            console.error("Failed to verify access token", error);
+            return next(new UnauthorizedException());
+        }
     }
-};
+}
