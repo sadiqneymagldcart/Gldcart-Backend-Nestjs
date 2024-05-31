a7ddb5ba8ebbb7134f4316701075efc5e779a9a2
sappChak <konotop401@gmail.com>
2024-05-31 16:13:45 +0200

    Add new watch collection changes method

--- src/services/chat/chat.service.ts
+++ src/services/chat/chat.service.ts
@@ -2,15 +2,18 @@ import { inject, injectable } from "inversify";
 import { Chat, ChatModel } from "@models/chat/Chat";
 import { BaseService } from "../base/base.service";
 import { Logger } from "@utils/logger";
+import { Nullable } from "@ts/types/nullable";
 
 @injectable()
 export class ChatService extends BaseService {
-
   public constructor(@inject(Logger) logger: Logger) {
     super(logger);
   }
+
   public async getChats(userId: string): Promise<Chat[]> {
-    return ChatModel.find({ participants: userId }).populate({
+    return await ChatModel.find({
+      participants: userId,
+    }).populate({
       path: "participants",
       select: { name: 1, surname: 1, type: 1, is_online: 1 },
     });
@@ -20,7 +23,7 @@ export class ChatService extends BaseService {
     await ChatModel.deleteMany({});
   }
 
-  public async checkChatForUsers(users: string[]): Promise<Chat | null> {
+  public async checkChatForUsers(users: string[]): Promise<Nullable<Chat>> {
     return ChatModel.findOne({
       participants: { $all: users },
     });
@@ -35,7 +38,23 @@ export class ChatService extends BaseService {
     return await ChatModel.create({ participants });
   }
 
-  public async getChatById(chatId: string): Promise<Chat | null> {
+  public async getChatById(chatId: string): Promise<Nullable<Chat>> {
     return ChatModel.findById(chatId);
+  }
+
+  public async watchChatCollectionChanges(onNewChat: (chat: any) => void) {
+    const changeStream = ChatModel.watch();
+    changeStream.on("change", async (change) => {
+      if (change.operationType === "insert") {
+        this.logger.logInfo("New chat created", change.documentKey._id);
+        const newChat = await ChatModel.findById(
+          change.documentKey._id,
+        ).populate({
+          path: "participants",
+          select: { name: 1, surname: 1, type: 1, is_online: 1 },
+        });
+        onNewChat(newChat);
+      }
+    });
   }
 }
