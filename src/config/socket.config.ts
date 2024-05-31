3c911c0e764c8641767f75dde9b52f432a6bb09e
sappChak <konotop401@gmail.com>
2024-05-31 16:12:57 +0200

    Declare config variables for sockets

---
+++ src/config/socket.config.ts
@@ -0,0 +1,33 @@
+const SocketConfig = Object.freeze({
+    PORT: 3000,
+    ORIGIN: "http://localhost:5173",
+    ALLOWED_HEADERS: "Origin, X-Requested-With, Content-Type, Accept",
+    ALLOWED_METHODS: "GET, POST",
+    ALLOWED_CREDENTIALS: true,
+    EVENTS: {
+        JOIN_ROOM: "join",
+        MESSAGE: "message",
+        LEAVE_ROOM: "leave",
+        DISCONNECT: "disconnect",
+    } 
+});
+
+const ServerOptions = {
+    cors: {
+        origin: SocketConfig.ORIGIN,
+        allowedHeaders: SocketConfig.ALLOWED_HEADERS,
+        methods: SocketConfig.ALLOWED_METHODS,
+        credentials: SocketConfig.ALLOWED_CREDENTIALS,
+    },
+}
+
+const ChatConfig = Object.freeze({
+    NAMESPACE: "chat",
+    EVENTS: {
+        CONNECTION: "connection",
+        NEW_CHAT: "newChat",
+        CHATS: "chats",
+    },
+});
+
+export { SocketConfig, ServerOptions, ChatConfig };
