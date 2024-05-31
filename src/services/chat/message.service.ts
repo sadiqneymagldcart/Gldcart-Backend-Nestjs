a8b451978b34945106c5a7be2ce9d8ce15c735a4
sappChak <konotop401@gmail.com>
2024-05-31 18:32:11 +0200

    Update imports

--- src/services/chat/message.service.ts
+++ src/services/chat/message.service.ts
@@ -1,41 +1,28 @@
 import { inject, injectable } from "inversify";
-import { Message, MessageModel } from "@models/chat/Message";
-import { Chat, ChatModel } from "@models/chat/Chat";
+import { MessageModel } from "@models/chat/Message";
 import { BaseService } from "../base/base.service";
 import { Logger } from "@utils/logger";
+import { IMessage } from "@ts/interfaces/IMessage";
+import { Nullable } from "@ts/types/nullable";
 
 @injectable()
 export class MessageService extends BaseService {
   public constructor(@inject(Logger) logger: Logger) {
     super(logger);
   }
-  public async createMessage(message: Message): Promise<Message> {
-    let chat: Chat | null;
 
-    if (!message.chatId) {
-      chat = await ChatModel.create({
-        participants: [message.senderId, message.recipientId],
-        messages: [],
-      });
-      message.chatId = chat._id;
-    } else {
-      chat = await ChatModel.findById(message.chatId);
-      if (!chat) throw new Error("Chat not found");
-    }
-
-    const savedMessage = await MessageModel.create(message);
-    await chat.save();
-
-    return savedMessage;
+  public async createMessage(message: IMessage): Promise<IMessage> {
+    return await MessageModel.create(message);
   }
-  public async getMessages(chatId: string): Promise<Message[]> {
-    return MessageModel.find({chatId}).sort({createdAt: 1});
+
+  public async getMessages(chatId: string): Promise<IMessage[]> {
+    return MessageModel.find({ chatId }).sort({ createdAt: 1 });
   }
 
   public async updateMessage(
     messageId: string,
-    message: Message,
-  ): Promise<Message | null> {
+    message: IMessage,
+  ): Promise<Nullable<IMessage>> {
     return MessageModel.findByIdAndUpdate(messageId, message, { new: true });
   }
 
