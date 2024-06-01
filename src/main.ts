0100be41255a92c52a41578e207be48b8bfac71c
sappChak <konotop401@gmail.com>
2024-06-01 23:49:04 +0200

    [refactor] Remove redundant method

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,11 @@ import { InversifyExpressServer } from "inversify-express-utils";
 import { Logger } from "@utils/logger";
 import { container } from "@config/inversify.config";
 
+
 const logger = container.get<Logger>(Logger);
 
 const express = new InversifyExpressServer(container);
 
-const app = new App(3001, logger, express);
+const app = new App(express, logger, 3001);
 
 app.start()
