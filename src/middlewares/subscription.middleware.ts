0d82dc6ac8216cf3e988812292df29ec366a1668
sappChak <konotop401@gmail.com>
2024-06-01 20:08:47 +0200

    Rework subscription middleware in inversify style

--- src/middlewares/subscription.middleware.ts
+++ src/middlewares/subscription.middleware.ts
@@ -1,37 +1,50 @@
-import { NextFunction, Response } from "express";
-import { UserModel } from "@models/user/User";
-import {UnauthorizedException} from "@exceptions/unauthorized.exception";
+import * as express from "express";
+import { UnauthorizedException } from "@exceptions/unauthorized.exception";
+import { BaseMiddleware } from "inversify-express-utils";
+import { inject, injectable } from "inversify";
+import { UserService } from "@services/user/user.service";
 
-export const requireSubscription = (requiredSubscriptionType: string) => {
-    return async (
-        _request: Request,
-        response: Response,
-        next: NextFunction,
-    ): Promise<void> => {
-        try {
-            const userId = response.locals.user.userId;
+@injectable()
+class SubscriptionMiddlware extends BaseMiddleware {
+    private readonly userService: UserService;
+    private requiredSubscriptionType: string;
 
-            if (!userId) {
-                return next(new UnauthorizedException());
-            }
+    public constructor(
+        @inject(UserService) userService: UserService,
+        requiredSubscriptionType: string,
+    ) {
+        super();
+        this.userService = userService;
+        this.requiredSubscriptionType = requiredSubscriptionType;
+    }
+    public async handler(
+        _request: express.Request,
+        response: express.Response,
+        next: express.NextFunction,
+    ) {
+        const userId = response.locals.user.userId;
 
-            const user =
-                await UserModel.findById(userId).populate("activeSubscription");
-
-            if (!user || !user.activeSubscription) {
-                response.status(403).json({ error: "No active subscription found" });
-            }
-
-            const userSubscriptionType = user?.activeSubscription;
-
-            if (userSubscriptionType !== requiredSubscriptionType) {
-                response
-                    .status(403)
-                    .json({ error: "Access denied for this subscription type" });
-            }
-            return next();
-        } catch (error: any) {
-            response.status(500).json({ error: "Internal server error" });
+        if (!userId) {
+            return next(new UnauthorizedException());
         }
-    };
-};
+
+        const user = await this.userService.getUserByIdAndPopulate(
+            userId,
+            "activeSubscription",
+        );
+
+        if (!user || !user.activeSubscription) {
+            response.status(403).json({ error: "No active subscription found" });
+        }
+
+        const userSubscriptionType = user?.activeSubscription;
+
+        if (userSubscriptionType !== this.requiredSubscriptionType) {
+            response
+                .status(403)
+                .json({ error: "Access denied for this subscription type" });
+        }
+        next();
+    }
+}
+export { SubscriptionMiddlware };
