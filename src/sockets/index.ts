dc79108328c37a71596351296b20e76f99043256
sappChak <konotop401@gmail.com>
2024-06-03 19:24:55 +0200

    Fix small issues, improve logging

--- src/sockets/index.ts
+++ src/sockets/index.ts
@@ -1,9 +1,10 @@
 import { Container } from "inversify/lib/container/container";
 import { ChatSocket } from "./chat.socket";
+import { Logger } from "@utils/logger";
 
-function startSockets(container: Container)
-{
+function startSockets(container: Container, logger: Logger) {
   container.get<ChatSocket>(ChatSocket);
+  logger.logInfo("⚡️[socket]: Sockets started");
 }
 
 export { startSockets };
