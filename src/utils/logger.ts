eafbc2227ad1b774aeb1bf0e700af757d416630e
sappChak <konotop401@gmail.com>
2024-06-03 16:08:32 +0200

    Update imports

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,8 +1,8 @@
 import * as winston from "winston";
 import { format } from "winston";
 import winstonDailyRotateFile from "winston-daily-rotate-file";
-import { injectable } from "inversify";
 import { ILogger } from "@interfaces/ILogger";
+import { injectable } from "inversify";
 
 export type LogMessage = string;
 
