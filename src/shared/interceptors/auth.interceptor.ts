e3b853c1d7446db97617cff4561495e0556ce550
sappChak <konotop401@gmail.com>
2024-06-23 18:13:03 +0200

    Create an interceptor for authorization

---
+++ src/shared/interceptors/auth.interceptor.ts
@@ -0,0 +1,37 @@
+import { AuthResponseDto } from '@auth/dto/auth.response.dto';
+import { setRefreshTokenCookie } from '@common/utils/auth.response.util';
+import {
+        CallHandler,
+        ExecutionContext,
+        Logger,
+        NestInterceptor,
+} from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export class AuthInterceptor implements NestInterceptor {
+        private readonly logger: Logger = new Logger(AuthInterceptor.name);
+
+        public intercept(
+                context: ExecutionContext,
+                next: CallHandler,
+        ): Observable<any> {
+                const request = context.switchToHttp().getRequest();
+                const response = context.switchToHttp().getResponse();
+
+                this.logger.log(
+                        `REST request to ${request.url}: ${JSON.stringify(request.body)}`,
+                );
+
+                return next.handle().pipe(
+                        map((data) => {
+                                if (data.refreshToken) {
+                                        setRefreshTokenCookie(response, data.refreshToken);
+                                        return plainToInstance(AuthResponseDto, data);
+                                }
+                                return data;
+                        }),
+                );
+        }
+}
