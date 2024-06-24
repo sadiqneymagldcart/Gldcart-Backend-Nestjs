c1b3721bfc5a5f8ddfe50d4d201f7487c8e22cc2
sappChak <konotop401@gmail.com>
2024-06-24 18:22:28 +0200

    Basic CRUD implementation

---
+++ src/modules/review/dto/create-review.dto.ts
@@ -0,0 +1,37 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNotEmpty,
+  IsMongoId,
+  IsNumber,
+  IsString,
+  Min,
+  Max,
+} from 'class-validator';
+
+export class CreateReviewDto {
+  @ApiProperty({ description: 'User ID who created the review' })
+  @IsNotEmpty()
+  @IsMongoId()
+  userId: string;
+
+  @ApiProperty({ description: 'Product ID that the review is for' })
+  @IsNotEmpty()
+  @IsMongoId()
+  productId: string;
+
+  @ApiProperty({
+    description: 'Rating given by the user',
+    minimum: 1,
+    maximum: 5,
+  })
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(1)
+  @Max(5)
+  rating: number;
+
+  @ApiProperty({ description: 'Text content of the review' })
+  @IsNotEmpty()
+  @IsString()
+  comment: string;
+}
--- src/modules/review/services/review.service.ts
+++ src/modules/review/services/review.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { CreateReviewDto } from '@review/dto/create-review.dto';
+import { UpdateReviewDto } from '@review/dto/update-review.dto';
+import { Review, ReviewDocument } from '@review/schemas/review.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class ReviewService {}
+export class ReviewService {
+    constructor(
+        @InjectModel(Review.name) private reviewModel: Model<ReviewDocument>,
+    ) {}
+
+    async create(createReviewDto: CreateReviewDto): Promise<Review> {
+        const createdReview = new this.reviewModel(createReviewDto);
+        return createdReview.save();
+    }
+
+    async findAll(): Promise<Review[]> {
+        return this.reviewModel.find().exec();
+    }
+
+    async findOne(id: string): Promise<Review> {
+        const review = await this.reviewModel.findById(id).exec();
+        if (!review) {
+            throw new NotFoundException(`Review with ID ${id} not found`);
+        }
+        return review;
+    }
+
+    async update(id: string, updateReviewDto: UpdateReviewDto): Promise<Review> {
+        const existingReview = await this.reviewModel.findByIdAndUpdate(id, updateReviewDto, { new: true }).exec();
+        if (!existingReview) {
+            throw new NotFoundException(`Review with ID ${id} not found`);
+        }
+        return existingReview;
+    }
+
+    async remove(id: string): Promise<void> {
+        const result = await this.reviewModel.findByIdAndDelete(id).exec();
+        if (!result) {
+            throw new NotFoundException(`Review with ID ${id} not found`);
+        }
+    }
+}
+
