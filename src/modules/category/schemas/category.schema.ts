98adcf795110ed3764993f9a2068431a73aeffed
sappChak <konotop401@gmail.com>
2024-06-24 17:29:49 +0200

    Define category and subcategory schemas

--- src/modules/category/schemas/category.schema.ts
+++ src/modules/category/schemas/category.schema.ts
@@ -1,20 +1,35 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import mongoose from 'mongoose';
+import mongoose, { Document, ObjectId } from 'mongoose';
 import { Subcategory } from './subcategory.schema';
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform, Type } from 'class-transformer';
 
 export type CategoryDocument = Category & Document;
 
 @Schema({ timestamps: true })
-export class Category extends Document {
+export class Category {
+  @ApiProperty({
+    type: String,
+    description: 'The unique identifier of the category',
+  })
+  @Transform(({ value }) => value.toString())
+  _id: ObjectId;
+
+  @ApiProperty({
+    type: String,
+    description: 'The name of the category',
+    required: true,
+  })
   @Prop({ required: true })
   name: string;
 
-  @Prop({
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Subcategory',
-    required: true,
+  @ApiProperty({
+    type: [String],
+    description: 'The subcategories linked to this category',
   })
-  subcategory: Subcategory;
+  @Prop([{ type: mongoose.Schema.Types.ObjectId, ref: 'Subcategory' }])
+  @Type(() => Subcategory)
+  subcategories: Subcategory[];
 }
 
 export const CategorySchema = SchemaFactory.createForClass(Category);
