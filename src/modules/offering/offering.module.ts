ba10eef4a57a4f861b937068d3396bc23e28c0aa
sappChak <konotop401@gmail.com>
2024-06-25 15:20:10 +0200

    Import CacheModule and use it

--- src/modules/offering/offering.module.ts
+++ src/modules/offering/offering.module.ts
@@ -3,9 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Offering, OfferingSchema } from './schemas/offering.schema';
 import { OfferingController } from './controllers/offering.controller';
 import { OfferingService } from './services/offering.service';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
+    CacheModule.register(),
     MongooseModule.forFeature([
       { name: Offering.name, schema: OfferingSchema },
     ]),
