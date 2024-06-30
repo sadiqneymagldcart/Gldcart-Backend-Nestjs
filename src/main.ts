28ab120d8373c2244012901d4b37873a36d872bd
sappChak <konotop401@gmail.com>
2024-06-30 15:34:34 +0200

    Set log levels based on app's stage

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { setup } from '@config/setup';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger:
+      process.env.NODE_ENV === 'development'
+        ? ['log', 'debug', 'error', 'verbose', 'warn']
+        : ['error', 'warn', 'log'],
+  });
   const configService = app.get(ConfigService);
   setup(app);
   await app.listen(configService.get('API_PORT') || 3001);
