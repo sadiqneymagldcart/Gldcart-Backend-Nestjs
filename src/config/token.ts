eb72d500c41bbbb4ae5bcca20e0869d60a400e70
sappChak <konotop401@gmail.com>
2024-06-30 15:35:42 +0200

    Remove token configuration factory

--- src/config/token.ts
+++
@@ -1,8 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-export default registerAs('token', () => ({
-  accessSecret: process.env.ACCESS_SECRET || 'access-secret',
-  accessExpirationTime: process.env.ACCESS_EXPIRATION_TIME || '15m',
-  refreshSecret: process.env.REFRESH_SECRET || 'refresh-secret',
-  refreshExpirationTime: process.env.REFRESH_EXPIRATION_TIME || '7d',
-}));
