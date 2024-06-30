a4f48ac1f719ce2257e0b0ebb9971f5243b95548
sappChak <konotop401@gmail.com>
2024-06-30 21:49:21 +0200

    Fix cart mapping

--- src/modules/cart/schemas/cart.schema.ts
+++ src/modules/cart/schemas/cart.schema.ts
@@ -1,36 +1,51 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Transform } from 'class-transformer';
-import mongoose, { Document, ObjectId } from 'mongoose';
 import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, Min } from 'class-validator';
+import mongoose from 'mongoose';
 
-export type CartDocument = Cart & Document;
+export type CartDocument = Cart & mongoose.Document;
 
 class CartItem {
-  @ApiProperty({ description: 'ID of the product' })
+  @ApiProperty({
+    description: 'ID of the product',
+    example: '668177b0ac6c1a132e160a6b',
+  })
   @Transform(({ value }) => value.toString())
-  @Prop({ type: mongoose.Schema.Types.ObjectId, required: true })
-  productId: ObjectId;
+  @Prop({ required: true, type: mongoose.Types.ObjectId })
+  productId: mongoose.Types.ObjectId;
 
-  @ApiProperty({ description: 'Quantity of the product' })
-  @Prop({ required: true })
+  @ApiProperty({ description: 'Quantity of the product', example: 1 })
+  @Prop({ required: true, type: Number })
+  @IsInt()
+  @Min(1)
   quantity: number;
 }
 
-const CartItemSchema = SchemaFactory.createForClass(CartItem);
-
 @Schema({ timestamps: true })
 export class Cart {
-  @ApiProperty({ description: 'The unique identifier of the cart' })
+  @ApiProperty({
+    description: 'The unique identifier of the cart',
+    example: '668177b0ac6c1a132e160a6b',
+  })
   @Transform(({ value }) => value.toString())
-  _id: ObjectId;
+  _id: mongoose.Types.ObjectId;
 
-  @ApiProperty({ description: 'User ID associated with the cart' })
+  @ApiProperty({
+    description: 'User ID associated with the cart',
+    example: '66792047d6650afd5905252e',
+  })
+  @Prop({
+    required: true,
+    type: mongoose.Types.ObjectId,
+    ref: 'User',
+    index: true,
+  })
   @Transform(({ value }) => value.toString())
-  @Prop({ type: mongoose.Schema.Types.ObjectId, required: true })
-  userId: ObjectId;
+  userId: mongoose.Types.ObjectId;
 
   @ApiProperty({ description: 'Items in the cart', type: [CartItem] })
-  @Prop({ type: [CartItemSchema], required: true })
+  @Prop({ required: true, type: [CartItem] })
   items: CartItem[];
 }
 
