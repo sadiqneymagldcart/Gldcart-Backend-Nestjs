f7b0ad1c9d7936736ed85cbaf6eb3386ba957fc3
sappChak <konotop401@gmail.com>
2024-06-30 21:59:53 +0200

    Remove mongoose prefix

--- src/modules/review/schemas/review.schema.ts
+++ src/modules/review/schemas/review.schema.ts
@@ -1,25 +1,25 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Transform } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
-import mongoose from 'mongoose';
+import { Document, Types } from 'mongoose';
 
-export type ReviewDocument = Review & mongoose.Document;
+export type ReviewDocument = Review & Document;
 
 @Schema({ timestamps: true })
 export class Review {
   @ApiProperty({ description: 'The unique identifier of the review' })
   @Transform(({ value }) => value.toString())
-  _id: mongoose.Schema.Types.ObjectId;
+  _id: Types.ObjectId;
 
   @ApiProperty({ description: 'User ID who created the review' })
   @Transform(({ value }) => value.toString())
-  @Prop({ type: mongoose.Schema.Types.ObjectId, required: true })
-  userId: mongoose.Schema.Types.ObjectId;
+  @Prop({ type: Types.ObjectId, required: true })
+  userId: Types.ObjectId;
 
   @ApiProperty({ description: 'Product ID that the review is for' })
   @Transform(({ value }) => value.toString())
-  @Prop({ type: mongoose.Schema.Types.ObjectId, required: true })
-  productId: mongoose.Schema.Types.ObjectId;
+  @Prop({ type: Types.ObjectId, required: true })
+  productId: Types.ObjectId;
 
   @ApiProperty({ description: 'Rating given by the user' })
   @Prop({ required: true })
