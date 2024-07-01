23d25905c77b0e1586fdd14a897fc51453c5ef51
sappChak <konotop401@gmail.com>
2024-07-01 13:50:07 +0200

    Move files to related folders

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ProductService } from './product.service';
-import { ProductController } from './product.controller';
+import { ProductService } from './services/product.service';
+import { ProductController } from './controllers/product.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Product, ProductSchema } from './schemas/product.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
+  ],
   controllers: [ProductController],
   providers: [ProductService],
 })
-export class ProductModule {}
+export class ProductModule { }
