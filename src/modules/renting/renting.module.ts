e20b9a1132650e98a1786ae2460bf0b9bc8cd1f2
sappChak <konotop401@gmail.com>
2024-07-01 14:07:17 +0200

    Import CacheModule

--- src/modules/renting/renting.module.ts
+++ src/modules/renting/renting.module.ts
@@ -3,9 +3,11 @@ import { RentingService } from './services/renting.service';
 import { RentingController } from './controllers/renting.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Renting, RentingSchema } from './schemas/renting.schema';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
+    CacheModule.register(),
     MongooseModule.forFeature([{ name: Renting.name, schema: RentingSchema }]),
   ],
   controllers: [RentingController],
