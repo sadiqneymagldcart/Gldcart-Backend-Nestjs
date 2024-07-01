9f3bd3b82f4c0c4a830fd3bf05e7c2f1ebc69d1d
sappChak <konotop401@gmail.com>
2024-07-01 13:51:13 +0200

    Move files to related folders

--- src/modules/renting/renting.module.ts
+++ src/modules/renting/renting.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { RentingService } from './renting.service';
-import { RentingController } from './renting.controller';
+import { RentingService } from './services/renting.service';
+import { RentingController } from './controllers/renting.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Renting, RentingSchema } from './schemas/renting.schema';
 
