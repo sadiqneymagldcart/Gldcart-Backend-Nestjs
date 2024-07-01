b039e896f9e6f336323bb1c4d757bbdb1ae54c86
sappChak <konotop401@gmail.com>
2024-07-01 13:50:07 +0200

    Move files to related folders

--- src/modules/product/services/product.service.ts
+++ src/modules/product/services/product.service.ts
@@ -1,52 +1,26 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { CreateProductDto } from '@product/dto/create-product.dto';
-import { UpdateProductDto } from '@product/dto/update-product.dto';
-import { IProductService } from '@product/interfaces/product.service.interface';
-import { Product, ProductDocument } from '@product/schemas/product.schema';
-import { Model } from 'mongoose';
+import { Injectable } from '@nestjs/common';
+import { CreateProductDto } from '../dto/create-product.dto';
+import { UpdateProductDto } from '../dto/update-product.dto';
 
 @Injectable()
-export class ProductService implements IProductService {
-  public constructor(
-    @InjectModel(Product.name)
-    private readonly productModel: Model<ProductDocument>,
-  ) {}
-
-  public async create(createProductDto: CreateProductDto): Promise<Product> {
-    const createdProduct = new this.productModel(createProductDto);
-    return createdProduct.save();
+export class ProductService {
+  create(createProductDto: CreateProductDto) {
+    return 'This action adds a new product';
   }
 
-  public async findAll(): Promise<Product[]> {
-    return this.productModel.find().lean().exec();
+  findAll() {
+    return `This action returns all product`;
   }
 
-  public async findById(id: string): Promise<Product> {
-    const offering = await this.productModel.findById(id).lean().exec();
-    if (!offering) {
-      throw new NotFoundException(`Product with ID ${id} not found`);
-    }
-    return offering;
+  findOne(id: number) {
+    return `This action returns a #${id} product`;
   }
 
-  public async update(
-    id: string,
-    updateProductDto: UpdateProductDto,
-  ): Promise<Product> {
-    const existingProduct = await this.productModel
-      .findByIdAndUpdate(id, updateProductDto, { new: true })
-      .exec();
-    if (!existingProduct) {
-      throw new NotFoundException(`Product with ID ${id} not found`);
-    }
-    return existingProduct;
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
   }
 
-  public async remove(id: string): Promise<void> {
-    const result = await this.productModel.findByIdAndDelete(id).exec();
-    if (!result) {
-      throw new NotFoundException(`Product with ID ${id} not found`);
-    }
+  remove(id: number) {
+    return `This action removes a #${id} product`;
   }
 }
