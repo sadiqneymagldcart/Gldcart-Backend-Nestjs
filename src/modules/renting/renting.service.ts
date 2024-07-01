967e57756a3d72d7da5ae18943c49f9b5d73b64d
sappChak <konotop401@gmail.com>
2024-07-01 13:23:30 +0200

    Rename everything related to rental to renting

---
+++ src/modules/renting/renting.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { CreateRentingDto } from './dto/create-renting.dto';
+import { UpdateRentingDto } from './dto/update-renting.dto';
+
+@Injectable()
+export class RentingService {
+  create(createRentingDto: CreateRentingDto) {
+    return 'This action adds a new renting';
+  }
+
+  findAll() {
+    return `This action returns all renting`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} renting`;
+  }
+
+  update(id: number, updateRentingDto: UpdateRentingDto) {
+    return `This action updates a #${id} renting`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} renting`;
+  }
+}
