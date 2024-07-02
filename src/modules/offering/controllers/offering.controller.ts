896dd68560e79c3ee7223be7d9b0cc40e8d5c507
sappChak <konotop401@gmail.com>
2024-07-02 10:10:52 +0200

    Add search by filters and text with pagination

--- src/modules/offering/controllers/offering.controller.ts
+++ src/modules/offering/controllers/offering.controller.ts
@@ -8,75 +8,158 @@ import {
   Param,
   Delete,
   UseInterceptors,
+  HttpCode,
+  HttpStatus,
+  Query,
 } from '@nestjs/common';
-import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiBody,
+  ApiQuery,
+} from '@nestjs/swagger';
 import { CreateOfferingDto } from '@offering/dto/create-offering.dto';
+import { PaginatedResourceDto } from '@search/dto/paginated-resource.dto';
 import { UpdateOfferingDto } from '@offering/dto/update-offering.dto';
 import { Offering } from '@offering/schemas/offering.schema';
 import { OfferingService } from '@offering/services/offering.service';
+import { FilteringParams } from '@shared/decorators/filtering.decorator';
+import {
+  Pagination,
+  PaginationParams,
+} from '@shared/decorators/pagination.decorator';
 import { SerializeWith } from '@shared/decorators/serialize.decorator';
 
 @ApiTags('Proffesional Services')
 @Controller('offerings')
 @SerializeWith(Offering)
 export class OfferingController {
-  public constructor(private readonly offeringService: OfferingService) {}
+  public constructor(private readonly offeringService: OfferingService) { }
 
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
   @ApiOperation({ summary: 'Create an offering' })
   @ApiBody({ type: CreateOfferingDto })
   @ApiResponse({
     status: 201,
     description: 'The offering has been successfully created.',
   })
-  @Post()
   public async create(
     @Body() createOfferingDto: CreateOfferingDto,
   ): Promise<Offering> {
-    return this.offeringService.create(createOfferingDto);
+    return await this.offeringService.create(createOfferingDto);
   }
 
+  @Get()
+  @HttpCode(HttpStatus.OK)
   @ApiOperation({ summary: 'Get all offerings' })
   @ApiResponse({ status: 200, description: 'Return all offerings.' })
-  @Get()
   @UseInterceptors(CacheInterceptor)
   public async findAll(): Promise<Offering[]> {
-    return this.offeringService.findAll();
+    return await this.offeringService.findAll();
   }
 
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Get offerings with filters' })
+  @ApiQuery({
+    name: 'size',
+    required: true,
+    type: Number,
+    description: 'Size of items per page',
+  })
+  @ApiQuery({
+    name: 'page',
+    required: true,
+    type: Number,
+    description: 'Page number',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Successful retrieval of offerings',
+    type: PaginatedResourceDto,
+  })
+  @ApiResponse({ status: 400, description: 'Invalid input' })
+  @ApiResponse({ status: 500, description: 'Internal server error' })
+  @UseInterceptors(CacheInterceptor)
+  public async getByFilters(
+    @PaginationParams() paginationParams: Pagination,
+    @FilteringParams() filters: { [key: string]: any },
+  ) {
+    return await this.offeringService.getByFilters(paginationParams, filters);
+  }
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Get offerings by search text' })
+  @ApiQuery({
+    name: 'text',
+    required: true,
+    type: String,
+    description: 'Text to search for',
+  })
+  @ApiQuery({
+    name: 'size',
+    required: true,
+    type: Number,
+    description: 'Size of items per page',
+  })
+  @ApiQuery({
+    name: 'page',
+    required: true,
+    type: Number,
+    description: 'Page number',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Successful retrieval of offerings',
+    type: PaginatedResourceDto<Offering>,
+  })
+  public async getBySearchQuery(
+    @PaginationParams() paginationParams: Pagination,
+    @Query('text') text: string,
+  ) {
+    return this.offeringService.getBySearchQuery(paginationParams, text);
+  }
+
+  @Get(':id')
+  @HttpCode(HttpStatus.OK)
   @ApiOperation({ summary: 'Get an offering by ID' })
   @ApiResponse({
     status: 200,
     description: 'Return the offering with the given ID.',
   })
   @ApiResponse({ status: 404, description: 'Offering not found.' })
-  @Get(':id')
   @UseInterceptors(CacheInterceptor)
   public async findById(@Param('id') id: string): Promise<Offering> {
-    return this.offeringService.findById(id);
+    return await this.offeringService.findById(id);
   }
 
+  @Put(':id')
+  @HttpCode(HttpStatus.OK)
   @ApiOperation({ summary: 'Update an offering by ID' })
   @ApiResponse({
     status: 200,
     description: 'The offering has been successfully updated.',
   })
   @ApiResponse({ status: 404, description: 'Offering not found.' })
-  @Put(':id')
   public async update(
     @Param('id') id: string,
     @Body() updateOfferingDto: UpdateOfferingDto,
   ): Promise<Offering> {
-    return this.offeringService.update(id, updateOfferingDto);
+    return await this.offeringService.update(id, updateOfferingDto);
   }
 
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
   @ApiOperation({ summary: 'Delete an offering by ID' })
   @ApiResponse({
     status: 204,
     description: 'The offering has been successfully deleted.',
   })
   @ApiResponse({ status: 404, description: 'Offering not found.' })
-  @Delete(':id')
   public async remove(@Param('id') id: string): Promise<void> {
-    return this.offeringService.remove(id);
+    return await this.offeringService.remove(id);
   }
 }
