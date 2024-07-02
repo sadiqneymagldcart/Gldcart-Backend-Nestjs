3fd11c85dc4d09f47f2940adee4ff872a9ea92ad
sappChak <konotop401@gmail.com>
2024-07-02 18:51:01 +0200

    Redefine Chat schema

--- src/modules/chat/schemas/chat.schema.ts
+++ src/modules/chat/schemas/chat.schema.ts
@@ -1,1 +1,16 @@
-export class Chat {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Transform } from 'class-transformer';
+import { Document, Types } from 'mongoose';
+
+export type ChatDocument = Chat & Document;
+
+@Schema({ timestamps: true })
+export class Chat {
+  @Transform(({ value }) => value.toString())
+  _id: string;
+
+  @Prop({ type: [{ type: Types.ObjectId, ref: 'User' }], index: true })
+  participants: string[];
+}
+
+export const ChatSchema = SchemaFactory.createForClass(Chat);
