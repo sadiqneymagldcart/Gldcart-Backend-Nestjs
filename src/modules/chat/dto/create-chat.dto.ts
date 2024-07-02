12a4287c8ee9e7e2083023e4ef16e0272f267acd
sappChak <konotop401@gmail.com>
2024-07-02 21:34:17 +0200

    Declare new DTOs

--- src/modules/chat/dto/create-chat.dto.ts
+++ src/modules/chat/dto/create-chat.dto.ts
@@ -1,1 +1,6 @@
-export class CreateChatDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateChatDto {
+  @ApiProperty({ type: [String], required: true, example: ['66829081894baf6d7bca705b', '668432eece23b39866f297e9']})
+  participants: string[];
+}
