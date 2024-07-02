c57f9df12607f4b8466d0a38fca65b0335ccccbb
sappChak <konotop401@gmail.com>
2024-07-02 10:25:51 +0200

    Create a new module

--- src/modules/chat/services/chat.service.ts
+++ src/modules/chat/services/chat.service.ts
@@ -1,58 +1,26 @@
 import { CreateChatDto } from '@chat/dto/create-chat.dto';
-import { CreateMessageDto } from '@chat/dto/create-message.dto';
-import { Chat, ChatDocument } from '@chat/schemas/chat.schema';
-import { Message, MessageDocument } from '@chat/schemas/message.schema';
+import { UpdateChatDto } from '@chat/dto/update-chat.dto';
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { User, UserDocument } from '@user/schemas/user.schema';
-import { Model } from 'mongoose';
-import { Socket } from 'socket.io';
 
 @Injectable()
 export class ChatService {
-  public constructor(
-    @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
-    @InjectModel(Chat.name) private readonly chatModel: Model<ChatDocument>,
-    @InjectModel(Message.name)
-    private readonly messageModel: Model<MessageDocument>,
-  ) { }
-
-  public async createChat(chat: CreateChatDto): Promise<Chat> {
-    return await this.chatModel.create(chat);
+  create(createChatDto: CreateChatDto) {
+    return 'This action adds a new chat';
   }
 
-  public async updateUserOnlineStatus(
-    userId: string,
-    status: boolean,
-  ): Promise<void> {
-    await this.userModel.findOneAndUpdate(
-      { _id: userId },
-      { is_online: status },
-    );
+  findAll() {
+    return `This action returns all chat`;
   }
 
-  public async getUserChats(userId: string): Promise<any> {
-    return await this.chatModel.find({ participants: userId }).populate({
-      path: 'participants',
-      select: { name: 1, surname: 1, type: 1, is_online: 1 },
-    });
+  findOne(id: number) {
+    return `This action returns a #${id} chat`;
   }
 
-  public watchChatCollectionChanges(socket: Socket): void {
-    const changeStream = this.chatModel.watch();
-    changeStream.on('change', async (change) => {
-      let chat: any;
-      if (change.operationType === 'insert') {
-        chat = await this.chatModel.findById(change.documentKey._id).populate({
-          path: 'participants',
-          select: { name: 1, surname: 1, type: 1, is_online: 1 },
-        });
-      }
-      socket.emit('newChat', chat);
-    });
+  update(id: number, updateChatDto: UpdateChatDto) {
+    return `This action updates a #${id} chat`;
   }
 
-  public async createMessage(message: CreateMessageDto): Promise<Message> {
-    return await this.messageModel.create(message);
+  remove(id: number) {
+    return `This action removes a #${id} chat`;
   }
 }
