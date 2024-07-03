fd23c37d68de6d8fba4a962c364e58fc1e11e9d3
sappChak <konotop401@gmail.com>
2024-07-03 22:13:15 +0200

    Implement createCustomer and createIntent methods

--- src/modules/stripe/services/stripe.service.ts
+++ src/modules/stripe/services/stripe.service.ts
@@ -1,4 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Metadata } from '@stripe/interfaces/metadata.interface';
+import Stripe from 'stripe';
 
 @Injectable()
-export class StripeService {}
+export default class StripeService {
+  private stripe: Stripe;
+
+  private readonly logger = new Logger(StripeService.name);
+
+  public constructor(private readonly configService: ConfigService) {
+    this.stripe = new Stripe(this.configService.get('STRIPE_SECRET_KEY')!, {
+      apiVersion: this.configService.get('STRIPE_API_VERSION'),
+    });
+  }
+
+  public async createCustomer(name: string, email: string) {
+    return this.stripe.customers.create({
+      name,
+      email,
+    });
+  }
+
+  public async createPaymentIntent(
+    amount: number,
+    currency: string,
+    metadata: Metadata,
+    customerId: string,
+  ): Promise<Stripe.PaymentIntent> {
+    try {
+      const paymentIntent = await this.stripe.paymentIntents.create({
+        amount,
+        customer: customerId,
+        currency,
+        metadata,
+        confirm: false,
+      });
+      this.logger.log(`Payment intent created: ${paymentIntent.id}`);
+      return paymentIntent;
+    } catch (error: any) {
+      this.logger.error(
+        `Failed to create payment intent for customer ${customerId} with amount ${amount} ${currency}`,
+        error,
+      );
+      throw error;
+    }
+  }
+}
