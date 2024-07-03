1c69a8748cf1a34bf3eef6971dc43c39acd295f4
sappChak <konotop401@gmail.com>
2024-07-03 10:14:19 +0200

    Create a stripe customer while creating user

--- src/modules/user/services/user.service.ts
+++ src/modules/user/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Nullable } from '@shared/types/common';
+import StripeService from '@stripe/services/stripe.service';
 import { CreateUserDto } from '@user/dto/create-user.dto';
 import { UpdateUserDto } from '@user/dto/update-user.dto';
 import { User, UserDocument } from '@user/schemas/user.schema';
@@ -10,23 +11,35 @@ import { Model } from 'mongoose';
 export class UserService {
   public constructor(
     @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
-  ) {}
+    private readonly stripeService: StripeService,
+  ) { }
 
-  public async create(createUserDto: CreateUserDto): Promise<User> {
-    const user = new this.userModel(createUserDto);
+  public async create(userData: CreateUserDto): Promise<User> {
+    const stripeCustomer = await this.stripeService.createCustomer(
+      userData.name,
+      userData.email,
+    );
+    const user = new this.userModel({
+      ...userData,
+      stripeCustomerId: stripeCustomer.id,
+    });
     return user.save();
   }
 
-  public async updateOrCreate(user: CreateUserDto): Promise<User> {
-    return this.userModel.findOneAndUpdate({ email: user.email }, user, {
-      upsert: true,
-      new: true,
-    });
+  public async updateOrCreate(userData: CreateUserDto): Promise<User> {
+    return this.userModel.findOneAndUpdate(
+      { email: userData.email },
+      userData,
+      {
+        upsert: true,
+        new: true,
+      },
+    );
   }
 
-  public async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
+  public async update(id: string, userData: UpdateUserDto): Promise<User> {
     const existingUser = await this.userModel
-      .findByIdAndUpdate(id, updateUserDto, { new: true })
+      .findByIdAndUpdate(id, userData, { new: true })
       .exec();
     if (!existingUser) {
       throw new NotFoundException(`User with ID ${id} not found`);
@@ -35,11 +48,11 @@ export class UserService {
   }
 
   public async findAll(): Promise<User[]> {
-    return this.userModel.find().exec();
+    return this.userModel.find();
   }
 
   public async findById(id: string): Promise<User> {
-    const user = await this.userModel.findById(id).exec();
+    const user = await this.userModel.findById(id);
     if (!user) {
       throw new NotFoundException(`User with ID ${id} not found`);
     }
@@ -47,15 +60,13 @@ export class UserService {
   }
 
   public async findByEmail(email: string): Promise<Nullable<User>> {
-    return this.userModel
-      .findOne({
-        email,
-      })
-      .exec();
+    return this.userModel.findOne({
+      email,
+    });
   }
 
   public async remove(id: string): Promise<void> {
-    const result = await this.userModel.findByIdAndDelete(id).exec();
+    const result = await this.userModel.findByIdAndDelete(id);
     if (!result) {
       throw new NotFoundException(`User with ID ${id} not found`);
     }
