608605643153ea6cacc9c4c00186f12f0ba38bc5
sappChak <konotop401@gmail.com>
2024-07-03 22:14:28 +0200

    Basic controller implementation with Swagger specs.

--- src/modules/stripe/controllers/stripe.controller.ts
+++ src/modules/stripe/controllers/stripe.controller.ts
@@ -1,7 +1,35 @@
-import { Controller } from '@nestjs/common';
-import { StripeService } from '@stripe/services/stripe.service';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
+import { JwtAuthenticationGuard } from '@shared/guards/jwt.auth.guard';
+import { PaymentIntentDto } from '@stripe/dto/payment-intent.dto';
+import StripeService from '@stripe/services/stripe.service';
 
+@ApiTags('Stripe')
 @Controller('stripe')
 export class StripeController {
-  constructor(private readonly stripeService: StripeService) {}
+  public constructor(private readonly stripeService: StripeService) { }
+
+  @Post()
+  @UseGuards(JwtAuthenticationGuard)
+  @ApiOperation({ summary: 'Create a new payment intent' })
+  @ApiResponse({
+    status: 201,
+    description: 'Payment intent successfully created.',
+  })
+  @ApiResponse({ status: 400, description: 'Bad Request.' })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
+  @ApiBody({ type: PaymentIntentDto })
+  public async createPaymentIntent(
+    @Body() intent: PaymentIntentDto,
+    @Req() request: Request & { user: { stripeCustomerId: string } },
+  ) {
+    const paymentIntent = await this.stripeService.createPaymentIntent(
+      intent.amount,
+      intent.currency,
+      { order_id: intent.orderId },
+      request.user.stripeCustomerId,
+    );
+
+    return { client_secret: paymentIntent.client_secret };
+  }
 }
