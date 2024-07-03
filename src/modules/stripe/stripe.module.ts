ccd49e30fb18bb01f85d572dcb47aa180873869c
sappChak <konotop401@gmail.com>
2024-07-03 10:18:45 +0200

    Import TokenModule for authentication check

--- src/modules/stripe/stripe.module.ts
+++ src/modules/stripe/stripe.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { StripeService } from './services/stripe.service';
 import { StripeController } from './controllers/stripe.controller';
+import StripeService from './services/stripe.service';
+import { TokenModule } from '@token/token.module';
 
 @Module({
+  imports: [TokenModule],
   controllers: [StripeController],
   providers: [StripeService],
 })
-export class StripeModule {}
+export class StripeModule { }
