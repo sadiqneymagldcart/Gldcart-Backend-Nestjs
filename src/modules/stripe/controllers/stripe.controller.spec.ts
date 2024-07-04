219400dade9e52e843ab6f13291d75ed1170d297
sappChak <konotop401@gmail.com>
2024-07-04 13:55:26 +0200

    Update imports

--- src/modules/stripe/controllers/stripe.controller.spec.ts
+++ src/modules/stripe/controllers/stripe.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StripeController } from './stripe.controller';
-import StripeService from '@stripe/services/stripe.service';
+import { StripePaymentService } from '@stripe/services/stripe.service';
 
 describe('StripeController', () => {
   let controller: StripeController;
@@ -8,7 +8,7 @@ describe('StripeController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [StripeController],
-      providers: [StripeService],
+      providers: [StripePaymentService],
     }).compile();
 
     controller = module.get<StripeController>(StripeController);
