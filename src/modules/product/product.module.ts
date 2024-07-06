f83618442877765bb1f15e7cfc45ddc88fddf867
sappChak <konotop401@gmail.com>
2024-07-06 22:43:30 +0200

    refactor(cache): move CacheModule registration to global scope

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -3,11 +3,9 @@ import { ProductService } from './services/product.service';
 import { ProductController } from './controllers/product.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Product, ProductSchema } from './schemas/product.schema';
-import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
-    CacheModule.register(),
     MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
   ],
   controllers: [ProductController],
