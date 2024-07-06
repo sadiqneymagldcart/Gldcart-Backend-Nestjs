9941e3d8e6d532763c1d8323c00dbe879568a78a
sappChak <konotop401@gmail.com>
2024-07-06 20:50:05 +0200

    Add new DTO

--- src/modules/order/dto/create-order.dto.ts
+++ src/modules/order/dto/create-order.dto.ts
@@ -1,6 +1,38 @@
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsArray,
+  IsEnum,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+import { User } from '@user/schemas/user.schema';
+import { OrderStatus } from '@order/enums/order-status.enum';
+import { Item } from '@item/schemas/item.schema';
+
 export class CreateOrderDto {
-  customer: string;
-  items: string[];
+  @IsNotEmpty()
+  @Type(() => User)
+  customer: User;
+
+  @IsArray()
+  @IsNotEmpty()
+  @Type(() => Item)
+  items: Item[];
+
+  @IsNumber()
+  @IsOptional()
   total: number;
-  status: string;
+
+  @IsNumber()
+  @IsOptional()
+  discount: number;
+
+  @IsEnum(OrderStatus)
+  @IsOptional()
+  status: OrderStatus;
+
+  @IsNumber()
+  @IsNotEmpty()
+  trackNumber: number;
 }
