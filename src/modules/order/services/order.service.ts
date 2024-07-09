2d3e6094e806c6d4dd4c3908da0128a938d87ee3
sappChak <konotop401@gmail.com>
2024-07-09 08:44:49 +0200

    refactor(order.service): optimize code for readability and performance

--- src/modules/order/services/order.service.ts
+++ src/modules/order/services/order.service.ts
@@ -1,36 +1,35 @@
-import { ItemService } from '@item/services/item.service';
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { CreateOrderDto } from '@order/dto/create-order.dto';
+import { UpdateOrderDto } from '@order/dto/update-order.dto';
 import { Order, OrderDocument } from '@order/schemas/order.schema';
-import { Nullable } from '@shared/types/common';
 import { Model } from 'mongoose';
 
 @Injectable()
 export class OrderService {
-  private readonly logger = new Logger(OrderService.name);
-
   public constructor(
     @InjectModel(Order.name) private readonly orderModel: Model<OrderDocument>,
-    private readonly itemService: ItemService,
-  ) {}
+  ) { }
 
-  public async create(order: Order): Promise<Order> {
-    this.logger.log('Creating order');
-    const createdOrder = new this.orderModel(order);
-    return createdOrder.save();
+  public async createOrder(order: CreateOrderDto): Promise<Order> {
+    return new this.orderModel(order).save();
   }
 
-  public async getByIdWithPopulatedItems(id: string): Promise<Nullable<Order>> {
-    this.logger.log(`Retrieving order with populated items: ${id}`);
-    const order = await this.orderModel.findById(id);
-
-    if (!order) {
-      this.logger.error(`Order not found: ${id}`);
-      return null;
+  public async findOrderWithItemsById(id: string): Promise<Order> {
+    const cart = await this.orderModel
+      .findById(id)
+      .populate('items.id')
+      .select('');
+    if (!cart) {
+      throw new NotFoundException(`Cart with ID ${id} not found`);
     }
+    return cart;
+  }
 
-    // const populatedItems = await this.itemService.populateItems(order.items);
-
-    // return { ...order.toJSON(), items: populatedItems };
+  public async updateOrder(
+    id: string,
+    data: UpdateOrderDto,
+  ): Promise<Order | null> {
+    return this.orderModel.findByIdAndUpdate(id, data, { new: true });
   }
 }
