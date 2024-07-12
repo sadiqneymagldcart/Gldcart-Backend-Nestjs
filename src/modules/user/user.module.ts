7ed54570c6111714024ff532c30bb6945dd5dbeb
sappChak <konotop401@gmail.com>
2024-07-12 16:37:26 +0200

    feat(user-module): add ProfileController and ProfileService

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -5,14 +5,17 @@ import { AuthModule } from '@auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
 import { StripeService } from '@stripe/services/stripe.service';
+import { ProfileController } from './controllers/profile.controller';
+import { AwsStorageService } from '@storages/services/storages.service';
+import { ProfileService } from './services/profile.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     forwardRef(() => AuthModule),
   ],
-  providers: [UserService, StripeService],
-  controllers: [UserController],
+  providers: [UserService, StripeService, AwsStorageService, ProfileService],
+  controllers: [UserController, ProfileController],
   exports: [UserService],
 })
 export class UserModule {}
