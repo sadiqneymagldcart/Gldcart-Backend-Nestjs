bb99be07222afcceb3524f134b9f570980d55e0d
sappChak <konotop401@gmail.com>
2024-07-13 15:33:42 +0200

    refactor(user): remove ProfileService

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -7,15 +7,14 @@ import { User, UserSchema } from './schemas/user.schema';
 import { StripeService } from '@stripe/services/stripe.service';
 import { ProfileController } from './controllers/profile.controller';
 import { AwsStorageService } from '@storages/services/storages.service';
-import { ProfileService } from './services/profile.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     forwardRef(() => AuthModule),
   ],
-  providers: [UserService, StripeService, AwsStorageService, ProfileService],
+  providers: [UserService, StripeService, AwsStorageService],
   controllers: [UserController, ProfileController],
   exports: [UserService],
 })
-export class UserModule {}
+export class UserModule { }
