58b83229f95f2a00498f242ff58cfc6912dc6cb7
sappChak <konotop401@gmail.com>
2024-07-13 14:17:41 +0200

    feat(address): add DTOs for creating and updating addresses

---
+++ src/modules/address/dto/create-address.dto.ts
@@ -0,0 +1,26 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateAddressDto {
+  @IsString()
+  @IsNotEmpty()
+  street: string;
+
+  @IsString()
+  @IsNotEmpty()
+  city: string;
+
+  @IsString()
+  @IsNotEmpty()
+  state: string;
+
+  @IsString()
+  @IsNotEmpty()
+  country: string;
+
+  @IsString()
+  @IsNotEmpty()
+  postalCode: string;
+
+  @IsNotEmpty()
+  user: string;
+}
