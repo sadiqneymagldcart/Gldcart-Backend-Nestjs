9d5ea1f83642a916ca175196a4df4e4b676e3b9b
sappChak <konotop401@gmail.com>
2024-07-13 15:34:30 +0200

    feat(user): add methods for managing shipping addresses

--- src/modules/user/services/user.service.ts
+++ src/modules/user/services/user.service.ts
@@ -1,3 +1,5 @@
+import { CreateAddressDto } from '@address/dto/create-address.dto';
+import { Address, AddressDocument } from '@address/schemas/address.schema';
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Nullable } from '@shared/types/common';
@@ -9,12 +11,13 @@ import { Model } from 'mongoose';
 
 @Injectable()
 export class UserService {
-  public constructor(
-    @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+  constructor(
+    @InjectModel(User.name)
+    private readonly userModel: Model<UserDocument>,
     private readonly stripeService: StripeService,
-  ) {}
+  ) { }
 
-  public async create(userData: CreateUserDto): Promise<User> {
+  public async createUser(userData: CreateUserDto): Promise<User> {
     const stripeCustomer = await this.stripeService.createCustomer(
       userData.name,
       userData.email,
@@ -27,7 +30,7 @@ export class UserService {
     return user;
   }
 
-  public async update(id: string, userData: UpdateUserDto): Promise<User> {
+  public async updateUser(id: string, userData: UpdateUserDto): Promise<User> {
     const existingUser = await this.userModel.findByIdAndUpdate(id, userData, {
       new: true,
     });
@@ -37,23 +40,23 @@ export class UserService {
     return existingUser;
   }
 
-  public async findAll(): Promise<User[]> {
-    return this.userModel.find().lean();
+  public async getAllUsers(): Promise<User[]> {
+    return this.userModel.find();
   }
 
-  public async findById(id: string): Promise<User> {
-    const user = await this.userModel.findById(id).lean();
+  public async getUserById(id: string): Promise<User> {
+    const user = await this.userModel.findById(id);
     if (!user) {
       throw new NotFoundException(`User with ID ${id} not found`);
     }
     return user;
   }
 
-  public async findByEmail(email: string): Promise<Nullable<User>> {
-    return this.userModel.findOne({ email }).lean();
+  public async getUserByEmail(email: string): Promise<Nullable<User>> {
+    return this.userModel.findOne({ email });
   }
 
-  public async remove(id: string): Promise<void> {
+  public async removeUser(id: string): Promise<void> {
     const result = await this.userModel.findOneAndDelete({ _id: id });
     if (!result) {
       throw new NotFoundException(`User with ID ${id} not found`);
@@ -77,5 +80,64 @@ export class UserService {
       throw new NotFoundException(`User with ID ${userId} not found`);
     }
     return user.stripeCustomerId;
+  }
+
+  public async updateProfilePicture(userId: string, image: string) {
+    const user = await this.userModel.findById(userId);
+    if (!user) {
+      throw new NotFoundException(`User with ID ${userId} not found`);
+    }
+    user.profile_picture = image;
+    return user.save();
+  }
+
+  public async getShippingAddresses(userId: string) {
+    const user = await this.userModel.findById(userId);
+    if (!user) {
+      throw new NotFoundException(`User with ID ${userId} not found`);
+    }
+    return user.shipping_addresses;
+  }
+
+  public async addShippingAddress(
+    userId: string,
+    newAddress: CreateAddressDto,
+  ) {
+    const user = await this.userModel.findById(userId);
+    if (!user) {
+      throw new NotFoundException(`User with ID ${userId} not found`);
+    }
+    user.shipping_addresses.push(newAddress);
+    return user.save().then((user) => user.shipping_addresses);
+  }
+
+  public async removeShippingAddress(userId: string, addressId: string) {
+    const user = await this.userModel.findById(userId);
+    if (!user) {
+      throw new NotFoundException(`User with ID ${userId} not found`);
+    }
+    user.shipping_addresses = user.shipping_addresses.filter(
+      (address: AddressDocument) => address._id?.toString() !== addressId,
+    );
+    return user.save().then((user) => user.shipping_addresses);
+  }
+
+  public async updateShippingAddress(
+    userId: string,
+    addressId: string,
+    updatedAddress: Address,
+  ) {
+    const user = await this.userModel.findById(userId);
+    if (!user) {
+      throw new NotFoundException(`User with ID ${userId} not found`);
+    }
+    const addressIndex = user.shipping_addresses.findIndex(
+      (address: AddressDocument) => address._id?.toString() === addressId,
+    );
+    if (addressIndex === -1) {
+      throw new NotFoundException(`Address with ID ${addressId} not found`);
+    }
+    user.shipping_addresses[addressIndex] = updatedAddress;
+    return user.save().then((user) => user.shipping_addresses);
   }
 }
