7f94a72e24ed7d34bacb14c6fac0f61cca9be3a3
sappChak <konotop401@gmail.com>
2024-07-27 09:33:10 +0200

    feat(payment-intent-dto): enhance PaymentIntentDto with validation and metadata

--- src/modules/stripe/dto/payment-intent.dto.ts
+++ src/modules/stripe/dto/payment-intent.dto.ts
@@ -1,9 +1,19 @@
-import { IsNumber, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class PaymentIntentDto {
+  @ApiProperty({ description: 'The amount to be paid' })
+  @IsNotEmpty()
+  @IsNumber()
   amount: number;
 
+  @ApiProperty({ description: 'The currency of the payment' })
+  @IsNotEmpty()
+  @IsString()
   currency: string;
 
-  order_id: string;
+  @ApiProperty({ description: 'Additional metadata for the payment' })
+  @IsNotEmpty()
+  @IsString()
+  metadata: string;
 }
