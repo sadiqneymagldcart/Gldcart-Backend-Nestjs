eeb47fdf54e19105401da46121e4aeac60fe7a6e
Andrii Konotop <konotop401@gmail.com>
2024-07-30 07:25:12 +0200

    fix(imports): reorder import statements in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
-import { AppModule } from './app.module';
 import { setupMiddlewares } from '@config/middlewares';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
