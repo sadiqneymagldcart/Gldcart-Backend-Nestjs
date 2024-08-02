37b79de9d83ec552b06f37d7581e3c142bcea98f
Andrii Konotop <konotop401@gmail.com>
2024-08-02 09:09:00 +0200

    feat(auth): add HttpModule

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -5,9 +5,10 @@ import { GoogleAuthController } from './controllers/google.auth.controller';
 import { AuthController } from './controllers/auth.controller';
 import { AuthService } from './services/auth.service';
 import { GoogleAuthService } from './services/google.auth.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [forwardRef(() => UserModule), TokenModule],
+  imports: [forwardRef(() => UserModule), TokenModule, HttpModule],
   controllers: [AuthController, GoogleAuthController],
   providers: [AuthService, GoogleAuthService],
 })
