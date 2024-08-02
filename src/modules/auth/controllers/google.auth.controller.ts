f010fdfb6c7bf0ef245147065165cdbefe9134f0
Andrii Konotop <konotop401@gmail.com>
2024-08-02 09:09:28 +0200

    refactor(auth): optimize GoogleAuthService for performance and readability

--- src/modules/auth/controllers/google.auth.controller.ts
+++ src/modules/auth/controllers/google.auth.controller.ts
@@ -1,12 +1,4 @@
-import {
-  BadRequestException,
-  Controller,
-  Get,
-  Logger,
-  Query,
-  Res,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { Controller, Get, Logger, Query, Res } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiQuery } from '@nestjs/swagger';
 import { Response } from 'express';
 import { GoogleAuthService } from '@auth/services/google.auth.service';
@@ -19,7 +11,7 @@ export class GoogleAuthController {
 
   public constructor(private readonly googleAuthService: GoogleAuthService) { }
 
-  @Get('/tokens/oauth/google')
+  @Get('tokens/oauth/google')
   @ApiOperation({ summary: 'Google OAuth Callback' })
   @ApiQuery({
     name: 'code',
@@ -37,43 +29,9 @@ export class GoogleAuthController {
     @Query('state') state: string,
     @Res() response: Response,
   ) {
-    this.logger.debug(
-      'Received OAuth callback with code: ' + code + ' and state: ' + state,
-    );
-
-    const oAuthTokens = await this.googleAuthService.getGoogleOAuthTokens(code);
-    if (!oAuthTokens) {
-      throw new BadRequestException('Invalid OAuth tokens');
-    }
-
-    this.logger.debug('Received OAuth tokens: ' + JSON.stringify(oAuthTokens));
-
-    const googleUser = await this.googleAuthService.getGoogleUser(
-      oAuthTokens.id_token,
-      oAuthTokens.access_token,
-    );
-    if (!googleUser) {
-      throw new UnauthorizedException('Google user not found');
-    }
-
-    const userDto = {
-      role: state,
-      name: googleUser.given_name,
-      surname: googleUser.family_name,
-      email: googleUser.email,
-      picture: googleUser.picture,
-      password: process.env.GOOGLE_PASSWORD!,
-    };
-
-    const result = await this.googleAuthService.loginGoogleUser(userDto);
-    if (!result.refreshToken) {
-      this.logger.error('Invalid credentials, no refresh token obtained');
-      throw new UnauthorizedException('Invalid credentials');
-    }
-
+    const result = await this.googleAuthService.handleGoogleAuth(code, state);
     this.logger.debug('Setting refresh token cookie and redirecting user');
     setRefreshTokenCookie(response, result.refreshToken);
-
     const redirectURL = process.env.CLIENT_URL!;
     return response.redirect(redirectURL);
   }
