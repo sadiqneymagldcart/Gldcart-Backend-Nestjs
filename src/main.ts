985c15538305f80217f8a833aa308d8252df8cf9
Andrii Konotop <konotop401@gmail.com>
2024-08-04 17:22:20 +0200

    refactor(main): simplify logger configuration and port retrieval

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { LogLevel } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { ConfigService } from '@nestjs/config';
 import { setupMiddlewares } from '@config/middlewares';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const loggerOptions: LogLevel[] =
+    process.env.NODE_ENV === 'development'
+      ? ['log', 'debug', 'error', 'verbose', 'warn']
+      : ['error', 'warn', 'log'];
+
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    logger:
-      process.env.NODE_ENV === 'development'
-        ? ['log', 'debug', 'error', 'verbose', 'warn']
-        : ['error', 'warn', 'log'],
+    logger: loggerOptions,
     rawBody: true,
   });
-  const port = app.get(ConfigService).get('PORT');
+
   setupMiddlewares(app);
-  app.listen(port);
+  app.listen(process.env.PORT);
 }
 
 bootstrap();
