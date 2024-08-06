d9bc1dfb3df2db6115a4b2d0dd0d750759f8f14e
Andrii Konotop <konotop401@gmail.com>
2024-08-06 17:02:50 +0200

    feat(logging): add debug logs to TransactionInterceptor for detailed transaction tracking

--- src/shared/interceptors/transaction.interceptor.ts
+++ src/shared/interceptors/transaction.interceptor.ts
@@ -1,12 +1,19 @@
-import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import {
+  CallHandler,
+  ExecutionContext,
+  NestInterceptor,
+  Logger,
+} from '@nestjs/common';
 import { InjectConnection } from '@nestjs/mongoose';
 import { Observable, catchError, tap } from 'rxjs';
 import mongoose from 'mongoose';
 
 export class TransactionInterceptor implements NestInterceptor {
+  private readonly logger = new Logger(TransactionInterceptor.name);
+
   public constructor(
     @InjectConnection() private readonly connection: mongoose.Connection,
-  ) {}
+  ) { }
 
   public async intercept(
     context: ExecutionContext,
@@ -19,6 +26,8 @@ export class TransactionInterceptor implements NestInterceptor {
 
     request.mongooseSession = session;
 
+    this.logger.debug('MongoDB session started and transaction initiated');
+
     session.startTransaction();
     return next.handle().pipe(
       tap(() => this.commitTransaction(session)),
@@ -27,13 +36,17 @@ export class TransactionInterceptor implements NestInterceptor {
   }
 
   private async commitTransaction(session: mongoose.ClientSession) {
+    this.logger.debug('Committing transaction');
     await session.commitTransaction();
     await session.endSession();
+    this.logger.debug('Transaction committed and session ended');
   }
 
   private async abortTransaction(session: mongoose.ClientSession, error: any) {
+    this.logger.debug('Aborting transaction due to error:', error);
     await session.abortTransaction();
     await session.endSession();
+    this.logger.debug('Transaction aborted and session ended');
     throw error;
   }
 }
