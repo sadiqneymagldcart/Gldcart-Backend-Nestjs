7a8e925fec8bd40b4b1b4c63fdd7aacf5c6b64e7
Andrii Konotop <konotop401@gmail.com>
2024-08-10 21:20:17 +0200

    feat(guards): refactor JwtAuthenticationGuard to use async/await for token verification

--- src/shared/guards/jwt.auth.guard.ts
+++ src/shared/guards/jwt.auth.guard.ts
@@ -4,20 +4,13 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
-import { Observable } from 'rxjs';
 import { TokenService } from '@token/services/token.service';
 
 @Injectable()
 export class JwtAuthenticationGuard implements CanActivate {
-  private readonly tokenService: TokenService;
+  public constructor(private readonly tokenService: TokenService) {}
 
-  public constructor(tokenService: TokenService) {
-    this.tokenService = tokenService;
-  }
-
-  public canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  public async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const authorization = request.headers['authorization'];
 
@@ -29,7 +22,7 @@ export class JwtAuthenticationGuard implements CanActivate {
       throw new UnauthorizedException('Invalid token');
     }
     try {
-      request.user = this.tokenService.verifyAccessToken(token);
+      request.user = await this.tokenService.verifyAccessToken(token);
     } catch (error) {
       throw new UnauthorizedException('Invalid token');
     }
