6394bc67e9d4394d53a28a603c65ad93ec8fd19f
Andrii Konotop <konotop401@gmail.com>
2024-08-10 21:23:17 +0200

    feat(order): integrate StripeService and TokenModule into OrderModule

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -6,14 +6,17 @@ import { InventoryService } from '@inventory/services/inventory.service';
 import { OrderService } from './services/order.service';
 import { OrderController } from './controllers/order.controller';
 import { Order, OrderSchema } from './schemas/order.schema';
+import { StripeService } from '@stripe/services/stripe.service';
+import { TokenModule } from '@token/token.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
+    TokenModule,
     ProductModule,
   ],
   controllers: [OrderController],
-  providers: [OrderService, EmailService, InventoryService],
+  providers: [OrderService, EmailService, InventoryService, StripeService],
   exports: [OrderService],
 })
 export class OrderModule {}
