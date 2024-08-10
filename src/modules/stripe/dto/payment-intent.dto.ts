66cbe05177a7bf0f219be6f7a58e2ec008874ee9
Andrii Konotop <konotop401@gmail.com>
2024-08-10 21:21:19 +0200

    refactor(stripe): rename Metadata interface to StripeMetadata and add Swagger examples

--- src/modules/stripe/dto/payment-intent.dto.ts
+++ src/modules/stripe/dto/payment-intent.dto.ts
@@ -1,22 +1,28 @@
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
-import { Metadata } from '@stripe/interfaces/metadata.interface';
+import { StripeMetadata } from '@stripe/interfaces/metadata.interface';
 
 export class PaymentIntentDto {
-  @ApiProperty({ description: 'The amount to be paid' })
-  @IsNotEmpty()
+  @ApiProperty({ description: 'The amount to be paid', example: 228 })
   @IsNumber()
   amount: number;
 
-  @ApiProperty({ description: 'The currency of the payment' })
-  @IsNotEmpty()
+  @ApiProperty({ description: 'The currency of the payment', example: 'usd' })
   @IsString()
   currency: string;
 
   @ApiProperty({
     description: 'Additional metadata for the payment',
     type: 'object',
+    example: {
+      billing_details: {
+        name: 'John Doe',
+        email: 'john.doe@example.com',
+        address: '123 Main St, Anytown, USA',
+      },
+      order_notes: 'Please deliver between 9 AM and 5 PM',
+    },
   })
   @IsNotEmpty()
-  metadata: Metadata;
+  metadata: StripeMetadata;
 }
