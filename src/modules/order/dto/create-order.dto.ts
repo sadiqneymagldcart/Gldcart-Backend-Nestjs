0ebea8ca8bde03060a9cbf272d51e81b666d9357
Andrii Konotop <konotop401@gmail.com>
2024-08-10 21:22:50 +0200

    docs(order): add Swagger examples to CreateOrderDto

--- src/modules/order/dto/create-order.dto.ts
+++ src/modules/order/dto/create-order.dto.ts
@@ -1,33 +1,43 @@
-import {
-  IsNotEmpty,
-  IsNumber,
-  IsOptional,
-  IsArray,
-  IsEnum,
-} from 'class-validator';
-import { OrderStatus } from '@order/enums/order-status.enum';
+import { IsNotEmpty, IsNumber, IsArray } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { Item } from '@item/schemas/item.schema';
 
 export class CreateOrderDto {
-  @IsNotEmpty()
+  @ApiProperty({
+    description: 'The unique identifier of the customer',
+    example: '66b7b3e8c32055a049b55029',
+  })
   customer: string;
 
+  @ApiProperty({
+    description: 'List of item IDs',
+    example: ['66b7b5a8901d3287c0cf6ea6', '66b7b6ed6ede04186f84f77b'],
+  })
   @IsArray()
   @IsNotEmpty()
-  items: string[];
+  items: Item[];
 
+  @ApiProperty({
+    description: 'Total amount of the order',
+    example: 228.07,
+  })
   @IsNumber()
-  @IsOptional()
-  total: number;
+  amount: number;
 
-  @IsNumber()
-  @IsOptional()
-  discount: number;
-
-  @IsEnum(OrderStatus)
-  @IsOptional()
-  status: OrderStatus;
-
-  @IsNumber()
+  @ApiProperty({
+    description: 'Billing details of the customer',
+    example: {
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      address: '123 Main St, Anytown, USA',
+    },
+  })
   @IsNotEmpty()
-  trackNumber: number;
+  billing_details: any;
+
+  @ApiProperty({
+    description: 'Notes for the order',
+    example: 'Please deliver between 9 AM and 5 PM',
+  })
+  order_notes: any;
 }
