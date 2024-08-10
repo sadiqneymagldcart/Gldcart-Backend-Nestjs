e955d8c6d7cba148a3e3cd44f03cfc05f43d3176
Andrii Konotop <konotop401@gmail.com>
2024-08-10 21:21:35 +0200

    refactor(stripe): add logging and use metadata directly in createPaymentIntent

--- src/modules/stripe/controllers/stripe.controller.ts
+++ src/modules/stripe/controllers/stripe.controller.ts
@@ -2,22 +2,32 @@ import {
   Body,
   Controller,
   HttpStatus,
+  Logger,
   Post,
   Req,
   UseGuards,
 } from '@nestjs/common';
-import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiBody,
+  ApiBearerAuth,
+} from '@nestjs/swagger';
 import { JwtAuthenticationGuard } from '@shared/guards/jwt.auth.guard';
 import { PaymentIntentDto } from '@stripe/dto/payment-intent.dto';
 import { StripeService } from '@stripe/services/stripe.service';
 
 @ApiTags('Stripe')
+@ApiBearerAuth()
+@UseGuards(JwtAuthenticationGuard)
 @Controller('stripe')
 export class StripeController {
+  private readonly logger = new Logger(StripeController.name);
+
   public constructor(private readonly stripeService: StripeService) {}
 
   @Post('payment-intent')
-  @UseGuards(JwtAuthenticationGuard)
   @ApiOperation({ summary: 'Create a new payment intent' })
   @ApiBody({ type: PaymentIntentDto })
   @ApiResponse({
@@ -28,13 +38,14 @@ export class StripeController {
     @Body() intent: PaymentIntentDto,
     @Req() request: Request & { user: { stripe_cus_id: string } },
   ) {
+    this.logger.debug(
+      `Creating payment intent for user ${JSON.stringify(request.user)} and metadata ${JSON.stringify(intent.metadata)}`,
+    );
     const payment_intent = await this.stripeService.createPaymentIntent(
       intent.amount,
-      intent.currency,
-      { metadata: intent.metadata },
+      intent.metadata,
       request.user.stripe_cus_id,
     );
-
     return { client_secret: payment_intent.client_secret };
   }
 }
