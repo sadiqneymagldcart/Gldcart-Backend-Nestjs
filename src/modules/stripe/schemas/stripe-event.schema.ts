39e990bfdc6321cf2a4508bdaa066edd0751df86
Andrii Konotop <konotop401@gmail.com>
2024-08-11 11:27:37 +0200

    fix(stripe-event): rename _id to event_id to avoid index conflict

--- src/modules/stripe/schemas/stripe-event.schema.ts
+++ src/modules/stripe/schemas/stripe-event.schema.ts
@@ -1,12 +1,14 @@
-import { Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
 export type StripeEventDocument = StripeEvent & Document;
 
 @Schema({
   versionKey: false,
+  timestamps: true,
 })
 export class StripeEvent {
-  _id: string;
+  @Prop({ required: true, unique: true })
+  event_id: string;
 }
 
 export const StripeEventSchema = SchemaFactory.createForClass(StripeEvent);
