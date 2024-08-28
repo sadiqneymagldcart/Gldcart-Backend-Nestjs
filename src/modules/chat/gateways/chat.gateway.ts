98f2ae6739fcb3b574ebdf27d5eecd330d2704d7
Andrii Konotop <konotop401@gmail.com>
2024-08-28 12:18:10 +0200

    refactor(chat-gateway): improve readability and maintainability

--- src/modules/chat/gateways/chat.gateway.ts
+++ src/modules/chat/gateways/chat.gateway.ts
@@ -18,20 +18,23 @@ import { IChatGateway } from '@chat/interfaces/chat-gateway.interface';
 import { ChatService } from '@chat/services/chat.service';
 import { MessageService } from '@chat/services/message.service';
 
+const CLIENT_URL = process.env.CLIENT_URL;
+
 @Injectable()
 @WebSocketGateway({
   namespace: 'chat',
   cors: {
-    origin: process.env.CLIENT_URL,
+    origin: CLIENT_URL,
   },
 })
 export class ChatGateway
   implements OnGatewayConnection, OnGatewayDisconnect, IChatGateway
 {
-  @WebSocketServer() server: Server;
   private readonly logger = new Logger(ChatGateway.name, {
     timestamp: true,
   });
+
+  @WebSocketServer() server: Server;
 
   public constructor(
     private readonly chatService: ChatService,
@@ -40,35 +43,35 @@ export class ChatGateway
   ) {}
 
   public async handleConnection(
-    @ConnectedSocket() socket: Socket,
+    @ConnectedSocket() client: Socket,
   ): Promise<void> {
-    const accessToken = socket.handshake.query.accessToken as string;
+    const accessToken = client.handshake.query.accessToken as string;
     try {
       const { _id } = await this.tokenService.verifyAccessToken(accessToken);
-      socket.data.userId = _id;
+      client.data.userId = _id;
       this.logger.log(`User connected: ${_id}`);
-      await this.handleUserStatusChange(_id, true, socket);
+      await this.handleUserStatusChange(_id, true, client);
     } catch (error) {
-      this.handleError('Error handling connection', error, socket);
+      this.handleError('Error handling connection', error, client);
     }
   }
 
   public async handleDisconnect(
-    @ConnectedSocket() socket: Socket,
+    @ConnectedSocket() client: Socket,
   ): Promise<void> {
-    const userId = this.getUserId(socket);
+    const userId = this.getUserId(client);
     if (!userId) return;
     this.logger.log(`User disconnected: ${userId}`);
     try {
-      await this.handleUserStatusChange(userId, false, socket);
+      await this.handleUserStatusChange(userId, false, client);
     } catch (error) {
-      this.handleError('Error handling disconnection', error, socket);
+      this.handleError('Error handling disconnection', error, client);
     }
   }
 
   @SubscribeMessage(Events.JOIN)
   public async handleJoin(
-    @ConnectedSocket() socket: Socket,
+    @ConnectedSocket() client: Socket,
     @MessageBody() chatId: string,
   ): Promise<void> {
     if (!chatId) {
@@ -76,12 +79,12 @@ export class ChatGateway
       return;
     }
     try {
-      const userId = this.getUserId(socket);
-      socket.join(chatId);
+      const userId = this.getUserId(client);
+      client.join(chatId);
       // socket.emit(Events.JOIN_ACK, { message: 'Successfully joined chat' });
       this.server.to(chatId).emit(Events.USER_JOINED, { userId });
     } catch (error) {
-      this.handleError('Error handling join', error, socket);
+      this.handleError('Error handling join', error, client);
     }
   }
 
@@ -103,7 +106,7 @@ export class ChatGateway
 
   @SubscribeMessage(Events.REQUEST_ALL_MESSAGES)
   public async requestAllMessages(
-    @ConnectedSocket() socket: Socket,
+    @ConnectedSocket() client: Socket,
     @MessageBody() chatId: string,
   ): Promise<void> {
     if (!chatId) {
@@ -111,27 +114,27 @@ export class ChatGateway
       return;
     }
     const messages = await this.messageService.getChatMessages(chatId);
-    socket.emit(Events.SEND_ALL_MESSAGES, messages);
+    client.emit(Events.SEND_ALL_MESSAGES, messages);
   }
 
   @SubscribeMessage(Events.CREATE_CHAT)
   public async createChat(
-    @ConnectedSocket() socket: Socket,
+    @ConnectedSocket() client: Socket,
     @MessageBody() newChat: CreateChatDto,
   ): Promise<void> {
     try {
       const chat = await this.chatService.createChat(newChat);
       this.server.emit(Events.RECEIVE_CHAT, chat);
     } catch (error) {
-      this.handleError('Error creating chat', error, socket);
+      this.handleError('Error creating chat', error, client);
     }
   }
 
   @SubscribeMessage(Events.REQUEST_ALL_CHATS)
   public async requestAllChats(
-    @ConnectedSocket() socket: Socket,
+    @ConnectedSocket() client: Socket,
   ): Promise<void> {
-    const userId = this.getUserId(socket);
+    const userId = this.getUserId(client);
 
     if (!userId) {
       this.logger.warn('User tried to request all chats without userId');
@@ -139,12 +142,12 @@ export class ChatGateway
     }
 
     const chats = await this.chatService.getUserChats(userId);
-    socket.emit(Events.SEND_ALL_CHATS, chats);
+    client.emit(Events.SEND_ALL_CHATS, chats);
   }
 
   @SubscribeMessage(Events.LEAVE)
   public async handleLeave(
-    @ConnectedSocket() socket: Socket,
+    @ConnectedSocket() client: Socket,
     @MessageBody() chatId: string,
   ): Promise<void> {
     if (!chatId) {
@@ -152,33 +155,33 @@ export class ChatGateway
       return;
     }
     try {
-      const userId = this.getUserId(socket);
-      socket.leave(chatId);
+      const userId = this.getUserId(client);
+      client.leave(chatId);
       // socket.emit(Events.LEAVE_ACK, { message: 'Successfully left chat' });
       this.server.to(chatId).emit(Events.USER_LEFT, { userId });
     } catch (error) {
-      this.handleError('Error handling leave', error, socket);
+      this.handleError('Error handling leave', error, client);
     }
   }
 
-  private getUserId(socket: Socket): string {
-    return socket.data.userId;
+  private getUserId(client: Socket): string {
+    return client.data.userId;
   }
 
   private async handleUserStatusChange(
     userId: string,
     isOnline: boolean,
-    socket: Socket,
+    client: Socket,
   ): Promise<void> {
     await this.chatService.updateUserOnlineStatus(userId, isOnline);
     if (isOnline) {
-      await this.requestAllChats(socket);
-      this.chatService.watchChatCollectionChanges(socket);
+      await this.requestAllChats(client);
+      this.chatService.watchChatCollectionChanges(client);
     }
   }
 
-  private handleError(message: string, error: any, socket: Socket): void {
+  private handleError(message: string, error: any, client: Socket): void {
     this.logger.error(`${message}: ${error.stack}`);
-    socket.emit(Events.ERROR, { message: error.message });
+    client.emit(Events.ERROR, { message: error.message });
   }
 }
