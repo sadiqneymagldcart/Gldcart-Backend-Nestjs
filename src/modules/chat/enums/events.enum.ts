80c7b959dfe5bec2e653bf20ffc01e62153d19a9
Andrii Konotop <konotop401@gmail.com>
2024-08-28 12:42:21 +0200

    refactor(chat-gateway): improve readability of event enums

--- src/modules/chat/enums/events.enum.ts
+++ src/modules/chat/enums/events.enum.ts
@@ -1,17 +1,23 @@
 export enum Events {
+  // Connection Events
   JOIN = 'join',
+  LEAVE = 'leave',
   USER_JOINED = 'user-joined',
+  USER_LEFT = 'user-left',
+
+  // Message Events
   SEND_MESSAGE = 'send-message',
   RECEIVE_MESSAGE = 'receive-message',
   REQUEST_ALL_MESSAGES = 'request-all-messages',
   SEND_ALL_MESSAGES = 'send-all-messages',
+
+  // Chat Events
   CREATE_CHAT = 'create-chat',
   RECEIVE_CHAT = 'receive-chat',
   REQUEST_ALL_CHATS = 'request-all-chats',
   SEND_ALL_CHATS = 'send-all-chats',
-  LEAVE = 'leave',
-  USER_LEFT = 'user-left',
-  CHATS = 'chats',
   NEW_CHAT = 'new-chat',
+
+  // Error Events
   ERROR = 'error',
 }
