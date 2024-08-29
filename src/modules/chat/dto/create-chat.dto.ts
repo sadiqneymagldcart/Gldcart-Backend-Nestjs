ac7a79c732c0bb2806f6edd059ecdbc91fcb08bd
Andrii Konotop <konotop401@gmail.com>
2024-08-29 21:36:13 +0200

    fix(chat): update example participant IDs in CreateChatDto

--- src/modules/chat/dto/create-chat.dto.ts
+++ src/modules/chat/dto/create-chat.dto.ts
@@ -4,7 +4,7 @@ export class CreateChatDto {
   @ApiProperty({
     type: [String],
     required: true,
-    example: ['66829081894baf6d7bca705b', '668432eece23b39866f297e9'],
+    example: ['66b7b3e8c32055a049b55029', '66ba4b1a9c829e01ecc1a247'],
   })
   participants: string[];
 }
