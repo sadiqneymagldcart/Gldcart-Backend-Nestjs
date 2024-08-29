63a1c27086b2ab6230ea126ed2793e42b98580ad
Andrii Konotop <konotop401@gmail.com>
2024-08-29 22:59:40 +0200

    refactor(chat): add validation decorators to CreateChatDto

--- src/modules/chat/dto/create-chat.dto.ts
+++ src/modules/chat/dto/create-chat.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { ArrayNotEmpty, IsArray, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateChatDto {
   @ApiProperty({
@@ -6,5 +7,9 @@ export class CreateChatDto {
     required: true,
     example: ['66b7b3e8c32055a049b55029', '66ba4b1a9c829e01ecc1a247'],
   })
+  @IsArray()
+  @ArrayNotEmpty()
+  @IsString({ each: true })
+  @IsNotEmpty({ each: true })
   participants: string[];
 }
