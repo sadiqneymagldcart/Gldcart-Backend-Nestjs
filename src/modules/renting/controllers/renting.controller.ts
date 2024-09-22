3ed7750669c19ff580363a3c497fedc92cd5b404
Azam Dildar <azamdev00@gmail.com>
2024-09-22 17:40:29 +0500

    add rent

--- src/modules/renting/controllers/renting.controller.ts
+++ src/modules/renting/controllers/renting.controller.ts
@@ -1,3 +1,4 @@
+import { CacheInterceptor } from '@nestjs/cache-manager';
 import {
   Controller,
   Get,
@@ -6,20 +7,39 @@ import {
   Patch,
   Param,
   Delete,
+  UseInterceptors,
+  Logger,
+  UploadedFiles,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { ApiConsumes, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateRentingDto } from '@renting/dto/create-renting.dto';
 import { UpdateRentingDto } from '@renting/dto/update-renting.dto';
 import { RentingService } from '@renting/services/renting.service';
+import { AwsStorageService } from '@storages/services/aws-storage.service';
 
 @ApiTags('Rentings')
 @Controller('renting')
+@UseInterceptors(CacheInterceptor)
 export class RentingController {
-  public constructor(private readonly rentingService: RentingService) {}
+  private readonly logger = new Logger(RentingController.name);
+
+  public constructor(
+    private readonly rentingService: RentingService,
+    private readonly awsStorage: AwsStorageService,
+  ) {}
 
   @Post()
-  create(@Body() createRentingDto: CreateRentingDto) {
-    return this.rentingService.create(createRentingDto);
+  @UseInterceptors(FilesInterceptor('images'))
+  @ApiOperation({ summary: 'Create a rent' })
+  @ApiConsumes('multipart/form-data')
+  public async create(
+    @UploadedFiles() images: Array<Express.Multer.File>,
+    @Body() rent: CreateRentingDto,
+  ) {
+    const imageUrls = await this.awsStorage.uploadMultipleFiles(images);
+    const rentWithImages = { ...rent, images: imageUrls };
+    return this.rentingService.create(rentWithImages);
   }
 
   @Get()
