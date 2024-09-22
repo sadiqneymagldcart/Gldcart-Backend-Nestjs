04d1c27ddd1d9eebece9f84d3278544fcb1a11db
Azam Dildar <azamdev00@gmail.com>
2024-09-22 18:02:50 +0500

    update dto and add aws service

--- src/modules/renting/renting.module.ts
+++ src/modules/renting/renting.module.ts
@@ -3,13 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { RentingService } from './services/renting.service';
 import { RentingController } from './controllers/renting.controller';
 import { Renting, RentingSchema } from './schemas/renting.schema';
+import { AwsStorageService } from '@storages/services/aws-storage.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Renting.name, schema: RentingSchema }]),
   ],
   controllers: [RentingController],
-  providers: [RentingService],
+  providers: [RentingService, AwsStorageService],
   exports: [RentingService],
 })
 export class RentingModule {}
