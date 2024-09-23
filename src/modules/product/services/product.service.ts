e1afc699f424bade2aaa7f3e0476b0d8567c2926
Azam Dildar <azamdev00@gmail.com>
2024-09-23 12:58:22 +0500

    clear product cache after adding or removing the product

--- src/modules/product/services/product.service.ts
+++ src/modules/product/services/product.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateProductDto } from '@product/dto/create-product.dto';
@@ -6,6 +6,8 @@ import { UpdateProductDto } from '@product/dto/update-product.dto';
 import { Product, ProductDocument } from '@product/schemas/product.schema';
 import { SearchService } from '@search/services/search.service';
 import { Pagination } from '@shared/decorators/pagination.decorator';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class ProductService {
@@ -13,15 +15,39 @@ export class ProductService {
   public constructor(
     @InjectModel(Product.name)
     private readonly productModel: Model<ProductDocument>,
+    @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
   ) {
     this.searchService = new SearchService<ProductDocument>(productModel);
+  }
+
+  public async getCacheKeys(): Promise<string[]> {
+    const redisStore: any = this.cacheManager.store;
+
+    // Execute Redis 'keys' command to get all keys
+    return new Promise((resolve, reject) => {
+      redisStore.keys('*', (err, keys) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(keys);
+      });
+    });
+  }
+
+  public async clearCacheKeys(): Promise<void> {
+    const keys = await this.getCacheKeys();
+    keys.forEach(async (key: string) => await this.cacheManager.del(key));
   }
 
   public async createProduct(
     createProductDto: CreateProductDto,
   ): Promise<Product> {
     const createdProduct = new this.productModel(createProductDto);
-    return createdProduct.save();
+    const savedProduct = await createdProduct.save();
+
+    await this.clearCacheKeys();
+
+    return savedProduct;
   }
 
   public async getAllProducts(): Promise<Product[]> {
@@ -42,6 +68,10 @@ export class ProductService {
       [key: string]: any;
     } = {},
   ): Promise<Product[]> {
+    const cachedProducts = await this.cacheManager.get<Product[]>('products');
+
+    console.log('Cache Products : ', cachedProducts);
+
     return this.searchService.searchWithPaginationAndFilters(
       pagination,
       filters,
@@ -90,5 +120,6 @@ export class ProductService {
     if (!result) {
       throw new NotFoundException(`Product with ID ${id} not found`);
     }
+    await this.clearCacheKeys();
   }
 }
